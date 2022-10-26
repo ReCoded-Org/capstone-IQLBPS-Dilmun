62126c3857cf5caa3776b8af6c43cc1ca5bbadfe
Oussama Masri <oussamamasri16120@gmail.com>
2022-10-26 19:43:57 +0300

    Delete: Home Route to Avoid CI errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import {
-  HOME_ROUTE
-} from './Route';
+
 
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path={HOME_ROUTE} element={<Home />} />
     </Routes>
   );
 }
