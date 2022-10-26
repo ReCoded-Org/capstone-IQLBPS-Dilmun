daf898aa31ba653beaf0d196337510945b20ebda
Omar Bassatni <omarbassatni@gmail.com>
2022-10-26 14:22:39 +0300

    changed contactUs to a reusable function

--- src/services/contact.js
+++ src/services/contact.js
@@ -1,29 +1,11 @@
-import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
-export const ContactUs = () => {
-    const form = useRef();
+const contactUs = (e, templateParams) => {
+    e.preventDefault();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+    emailjs.sendForm('service_iwqsu3f', 'template_d0s4lc6', templateParams, 'SMrzCAgEF5PcNX7Jo')
 
-        emailjs.sendForm('service_iwqsu3f', 'template_d0s4lc6', form.current, 'SMrzCAgEF5PcNX7Jo')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
-
-    return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
-        </form>
-    );
+    e.target.reset()
 };
+
+export { contactUs }
