d716265105eb33c8f53e74ef0c90d9d4003b0cec
Sarah Abou Dalal <109027707+sarahaboudalal@users.noreply.github.com>
2022-10-26 20:35:32 +0300

    3 set up firebase (#47)
    * firebase installed
    
    * firebase config file added
    
    * comments deleted
    
    * config file moved

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGo679LUJ94eaT5O-L-k30CuPa4khqj1w",
+  authDomain: "capstone-dilmun.firebaseapp.com",
+  projectId: "capstone-dilmun",
+  storageBucket: "capstone-dilmun.appspot.com",
+  messagingSenderId: "382842867534",
+  appId: "1:382842867534:web:ab6d944d9c52d9f5e8581f"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db =getFirestore(app);
+
