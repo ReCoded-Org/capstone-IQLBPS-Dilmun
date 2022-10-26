addce3784f280964692d75b270fa77ff6a6d48d5
Omar Bassatni <omarbassatni@gmail.com>
2022-10-26 17:23:54 +0300

    added comments to contact.js

--- src/services/contact.js
+++ src/services/contact.js
@@ -1,11 +1,14 @@
+// Emailjs imports
 import emailjs from '@emailjs/browser';
 
+// function for sending emails that takes form event and form input values as arguments
+//check the template to know what the inputs name must be because they matter
 const mailSender = (e, templateParams) => {
-    e.preventDefault();
+    e.preventDefault(); //to stop the form from reloading the page onSubmit
 
-    emailjs.sendForm('service_iwqsu3f', 'template_d0s4lc6', templateParams, 'SMrzCAgEF5PcNX7Jo')
+    emailjs.sendForm('service_iwqsu3f', 'template_d0s4lc6', templateParams, 'SMrzCAgEF5PcNX7Jo') //emailjs method to send the form that takes the form inputs as a third argument
 
-    e.target.reset()
+    e.target.reset() //reset the form after the submit
 };
 
 export { mailSender }
