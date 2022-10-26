13d1a3f53a0e8d3a9c0a99175af409ed6eaa5e54
Omar Bassatni <omarbassatni@gmail.com>
2022-10-26 17:40:50 +0300

    fixing comments

--- src/services/contact.js
+++ src/services/contact.js
@@ -4,11 +4,14 @@ import emailjs from '@emailjs/browser';
 // function for sending emails that takes form event and form input values as arguments
 //check the template to know what the inputs name must be because they matter
 const mailSender = (e, templateParams) => {
-    e.preventDefault(); //to stop the form from reloading the page onSubmit
+    //to stop the form from reloading the page onSubmit
+    e.preventDefault();
 
-    emailjs.sendForm('service_iwqsu3f', 'template_d0s4lc6', templateParams, 'SMrzCAgEF5PcNX7Jo') //emailjs method to send the form that takes the form inputs as a third argument
+    //emailjs method to send the form that takes the form inputs as a third argument
+    emailjs.sendForm('service_iwqsu3f', 'template_d0s4lc6', templateParams, 'SMrzCAgEF5PcNX7Jo')
 
-    e.target.reset() //reset the form after the submit
+    //reset the form after the submit
+    e.target.reset()
 };
 
 export { mailSender }
