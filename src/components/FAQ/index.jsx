d3393d99dde9f658a93db2f6cf58410216d39871
Marwa Hakmi <marwahakmi@gmail.com>
2022-10-27 22:21:40 +0300

    refactor: file structure

---
+++ src/components/FAQ/index.jsx
@@ -0,0 +1,9 @@
+function FAQ() {
+  return (
+    <div>
+      <h1 className="font-bold text-secondary">FAQ Component</h1>
+    </div>
+  );
+}
+
+export default FAQ;
--- src/components/home/FAQ.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function FAQ() {
-  return <div>FAQ Component</div>;
-}
-
-export default FAQ;
