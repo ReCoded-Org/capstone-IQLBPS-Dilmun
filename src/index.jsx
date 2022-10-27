7905651f16d9ec62cef803a35c5c130b52b0efe0
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-27 17:49:24 +0300

    setup the i18next with two different langguage json file Relates #6

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import { store } from './app/store';
 import App from './App';
 import './index.css';
+
+import './i18n'
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
