67aa733e4e4749653054b2c31e361faee5aa3f49
Oussama Masri <oussamamasri16120@gmail.com>
2022-10-28 23:55:12 +0300

    Update: updated the navbar the style has been fixed, its responsive, contain some weird bugs still processing (phase1)

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link , NavLink } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 
 
 import{
@@ -8,25 +11,42 @@ import{
     SIGN_UP_ROUTE,
     SIGN_IN_ROUTE,
     LOG_OUT,
-    PROFILE,
-    FAQ_ROUTE
+    PROFILE
 } from '../../route'
 
 function NavBar(){
+
+     const [togglerNav, setTogglerNav] = useState(false);
+
+     const clickHandler = () => {
+         setTogglerNav(!togglerNav);
+       };
+
     return(
-            <nav className='bg-primary text-background px-4 md:text-2xl'>
-            <div className='grid grid-rows-10 grid-flow-col items-center gap-1 md:gap-0'>
-                <Link to={HOME_ROUTE} className='justify-self-start hover:text-tertiary'>Home</Link>
-                <Link to={CONTACT_ROUTE} className='justify-self-start hover:text-tertiary'>Contact</Link>
-                <Link to={ABOUT_ROUTE} className='justify-self-start hover:text-tertiary'>About</Link>
-                <Link to={PROFILE} className='justify-self-start hover:text-tertiary'>Profile</Link>
-                <img src='https://cdn.discordapp.com/attachments/1031834305703460906/1035591582444949564/dilmun-logo.png' className='object-scale-down h-20 justify-self-center py-0' alt='logo'/>
-                <Link to={SIGN_UP_ROUTE} className='justify-self-end hover:text-tertiary'>Sign Up</Link>
-                <Link to={SIGN_IN_ROUTE} className='justify-self-end hover:text-tertiary'>Sign In</Link>
-                <Link to={LOG_OUT} className='justify-self-end hover:text-tertiary'>Log Out</Link>
-                <Link to={FAQ_ROUTE} className='justify-self-end hover:text-tertiary'>FAQ</Link>
-                <button type='submit' className='mx-4 '><img src='https://cdn.discordapp.com/attachments/1031834305703460906/1035608527923843122/translation-icon.png' className='text-background h-8 justify-self-end  hover:text-tertiary' alt='icon'/></button>
-               </div>
+            <nav className=' h-auto md:h-24 p-2 bg-blue-200 max-w-10xl mx-auto flex justify-between md:items-center'>
+                <Link to={HOME_ROUTE}>
+                <img src='https://cdn.discordapp.com/attachments/1031834305703460906/1035627738440159303/Asset_23.png' className='object-scale-down h-20 justify-self-center py-0 mx-4' alt='logo'/>
+                </Link>
+                <div
+                 className={
+                togglerNav ? 'flex flex-col gap-4 md:inline' : 'hidden md:inline'
+                }
+                >
+                <NavLink className='nav-link' onClick={clickHandler} to={HOME_ROUTE} >Home</NavLink>
+                <NavLink className='nav-link' onClick={clickHandler} to={CONTACT_ROUTE} >Contact</NavLink>
+                <NavLink className='nav-link' onClick={clickHandler} to={ABOUT_ROUTE} >About</NavLink>
+                <NavLink className='nav-link' onClick={clickHandler} to={PROFILE} >Profile</NavLink>
+                <NavLink className='nav-link' onClick={clickHandler} to={SIGN_UP_ROUTE} >Sign Up</NavLink>
+                <NavLink className='nav-link' onClick={clickHandler} to={SIGN_IN_ROUTE} >Sign In</NavLink>
+                <NavLink className='nav-link' onClick={clickHandler} to={LOG_OUT} >Log Out</NavLink>
+                </div>
+                <button 
+        className=' inline md:hidden self-start nav-link'
+        type="button"
+        onClick={clickHandler}>
+        {togglerNav ? <AiOutlineClose /> : <FaBars />}
+      </button>
+        <button type='submit' className='mx-4'><img src='https://cdn.discordapp.com/attachments/1031834305703460906/1035608527923843122/translation-icon.png'alt='icon'/></button>
             </nav>
     )
 }
