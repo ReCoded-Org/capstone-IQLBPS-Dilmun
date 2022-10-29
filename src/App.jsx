79c9a9413d8b8fccd2fc0066802bd8019f6759ea
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-10-29 19:59:09 +0300

    importing navbar error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar/NavBar';    
+import NavBar from './components/NavBar/NavBar'    
 import NewsLetter from './components/NewsLetter';
 
 function App() {
