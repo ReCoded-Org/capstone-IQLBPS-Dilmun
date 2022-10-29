7ba4c4de5f63fc48650bdcc709c5d4d05b1c94e1
Omar Bassatni <omarbassatni@gmail.com>
2022-10-29 12:44:41 +0300

    added more tests

--- src/components/NewsLetter/NewsLetter.test.js
+++ src/components/NewsLetter/NewsLetter.test.js
@@ -3,9 +3,13 @@ import { render } from "@testing-library/react";
 import NewsLetter from ".";
 
 
-
 it('render correctly', () => {
-    const { queryByPlaceholderText } = render(<NewsLetter />)
+    const { queryByPlaceholderText, getByText, queryByAltText} = render(<NewsLetter />)
 
     expect(queryByPlaceholderText('Write your email here')).toBeTruthy()
+    expect(getByText('Subscribe to our newsletter and stay updated')).toBeTruthy()
+    expect(queryByAltText('email')).toBeTruthy()
+    expect(getByText("Subscribe to our Newsletter !")).toMatchSnapshot(`
+    <h1 className='text-[20px] sm:text-[40px] mb-3 sm:mb-0'>Subscribe to our <br /> Newsletter !</h1>
+  `);
 })
