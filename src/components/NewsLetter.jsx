b62dd734f1662bad23ea490606f60ae04865f1a1
Omar Bassatni <omarbassatni@gmail.com>
2022-10-29 10:48:13 +0300

    Changed text color and added padding

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const NewsLetter = () => {
   return (
-    <div className='m-14 p-4 md:flex rounded-md shadow-md justify-around bg-primary'>
-
-      <div className='text-white flex flex-col md:flex-row justify-center items-center'>
-        <img className='w-24 md:w-40  h-100' src="https://www.freepnglogos.com/uploads/blue-email-logo-png-4.png" alt="email" />
-        <h1 className='text-[20px] sm:text-[40px] mb-3 sm:mb-0'>Subscribe to our <br /> Newsletter !</h1>
-      </div>
-
-      <div className='mt-4'>
-        <p className='text-white mb-5'>Subscribe to our newsletter and stay updated</p>
-        <div className='items-center sm:flex justify-center gap-2'>
-          <input type="text" placeholder='Write your email here' className='rounded p-1 w-full sm:w-fit md:p-2 mb-3 sm:mb-0 placeholder:text-primary border border-solid ' />
-          <button type='submit' className='md:p-2 p-1 w-full sm:w-fit rounded text-primary bg-tertiary'>Subscribe</button>
+    <section>
+      <div className='m-14 pt-14 pb-14 md:flex rounded-md shadow-md justify-around bg-primary'>
+        <div className='text-background flex flex-col md:flex-row justify-center items-center'>
+          <img className='w-24 md:w-40  h-100' src="https://www.freepnglogos.com/uploads/blue-email-logo-png-4.png" alt="email" />
+          <h1 className='text-[20px] sm:text-[40px] mb-3 sm:mb-0'>Subscribe to our <br /> Newsletter !</h1>
+        </div>
+        <div className='mt-4'>
+          <p className='text-background mb-5'>Subscribe to our newsletter and stay updated</p>
+          <div className='items-center sm:flex justify-center gap-2'>
+            <input type="text" placeholder='Write your email here' className='rounded p-1 w-full sm:w-fit md:p-2 mb-3 sm:mb-0 placeholder:text-background border border-solid ' />
+            <button type='submit' className='md:p-2 p-1 w-full sm:w-fit rounded text-primary bg-tertiary'>Subscribe</button>
+          </div>
         </div>
       </div>
 
+    </section>
 
-    </div>
 
   )
 }
