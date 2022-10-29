f937ba9658e26abfcced08d614bbd87a1c6b38bd
ayubabdullah <55099143+ayubabdullah@users.noreply.github.com>
2022-10-29 20:18:56 +0300

    Update the background color of the feature section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -6,7 +6,7 @@ import FeatureCard from './FeatureCard';
 
 const Features = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-background dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
