7a0964e4bb1f6918323557cb54345c255513eafb
Omar Bassatni <omarbassatni@gmail.com>
2022-10-29 11:32:58 +0300

    added newsletter test in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
+// import { Provider } from "react-redux";
+// import { store } from "./app/store";
+// import App from "./App";
+import NewsLetter from "./components/NewsLetter";
 
-test("renders learn react link", () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+// test("renders learn react link", () => {
+//   const { getByText } = render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   );
+
+//   expect(getByText(/learn/i)).toBeInTheDocument();
+// });
+
+
+it('render correctly', () => {
+  const { queryByPlaceholderText } = render(<NewsLetter />)
+
+  expect(queryByPlaceholderText('Write your email here')).toBeTruthy()
+})
