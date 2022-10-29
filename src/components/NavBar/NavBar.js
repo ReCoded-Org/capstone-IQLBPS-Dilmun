8253ea36033e94b58258e3a70ba0faa16fd1c552
Oussama Masri <oussamamasri16120@gmail.com>
2022-10-29 22:38:04 +0300

    Fix: fixed the NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
+import {SiGoogletranslate} from 'react-icons/si'
 
-import{
+ import{
     HOME_ROUTE,
     CONTACT_ROUTE,
     ABOUT_ROUTE,
@@ -11,46 +12,41 @@ import{
     SIGN_IN_ROUTE,
     LOG_OUT,
     PROFILE
-} from '../../route'
+ } from '../../route'
 
 function NavBar(){
 
-     const [togglerNav, setTogglerNav] = useState(false);
-
-     const clickHandler = () => {
-         setTogglerNav(!togglerNav);
-       };
+    const [open,setOpen]=useState(false);
 
     return(
-            <nav id='validation' className=' h-auto md:h-24 p-2 bg-primary text-background max-w-10xl mx-auto flex flex-row justify-center md:items-center'
-            >
+     
+        <div id='validation' className='shadow-md w-full fixed top-0 left-0 bg-primary '>
+            <div className='md:flex bg-primary py-3 items-center justify-between md:px-8 px-5'>
+                <div >
                 <Link to={HOME_ROUTE}>
                 <img src='https://cdn.discordapp.com/attachments/1031834305703460906/1035627738440159303/Asset_23.png'
-                className='object-scale-down h-20 md:justify-self-center py-0 mx-12' alt='logo'/>
+                className='object-scale-down h-20 md:justify-self-center py-0' alt='logo'/>
                 </Link>
-                <div
-                 className={
-                togglerNav ? 'flex flex-col gap-4 md:inline' : 'hidden md:inline'
-                }
-                >
-                <Link className='nav-link hover:text-tertiary' to={HOME_ROUTE} >Home</Link>
-                <Link className='nav-link hover:text-tertiary' to={CONTACT_ROUTE} >Contact</Link>
-                <Link className='nav-link hover:text-tertiary' to={ABOUT_ROUTE} >About</Link>
-                <Link className='nav-link hover:text-tertiary' to={PROFILE} >Profile</Link>
-                <Link className='nav-link hover:text-tertiary' to={SIGN_UP_ROUTE} >Sign Up</Link>
-                <Link className='nav-link hover:text-tertiary' to={SIGN_IN_ROUTE} >Sign In</Link>
-                <Link className='nav-link hover:text-tertiary' to={LOG_OUT} >Log Out</Link>
-                <button type='submit' id='translation-icon'><img src='https://cdn.discordapp.com/attachments/1031834305703460906/1035608527923843122/translation-icon.png'
-                className='h-10 justify-self-center py-0 mx-12' alt='icon'/></button>
                 </div>
                 <button 
-                className=' inline md:hidden self-start nav-link'
-                type="button"
-                onClick={clickHandler}>
-                {togglerNav ? <AiOutlineClose /> : <FaBars />}
+                type='button'
+                onClick={()=>setOpen(!open)}>  
+                {open ? <AiOutlineClose className='text-5xl absolute right-8 top-6 cursor-pointer md:hidden'/> : <FaBars className='text-5xl absolute right-8 top-6 cursor-pointer md:hidden'/>}
                 </button>
-            </nav>
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-primary md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-32 ':'top-[-490px]'}`}>
+                    <li><Link className='md:ml-8 text-xl md:my-0 my-7 text-white hover:text-secondary duration-500' to={HOME_ROUTE} >Home</Link></li>
+                    <li><Link className='md:ml-8 text-xl md:my-0 my-7 text-white hover:text-secondary duration-500' to={CONTACT_ROUTE} >Contact</Link></li>
+                    <li><Link className='md:ml-8 text-xl md:my-0 my-7 text-white hover:text-secondary duration-500' to={ABOUT_ROUTE} >About</Link></li>
+                    <li><Link className='md:ml-8 text-xl md:my-0 my-7 text-white hover:text-secondary duration-500' to={PROFILE} >Profile</Link></li>
+                    <li><Link className='md:ml-8 text-xl md:my-0 my-7 text-white hover:text-secondary duration-500' to={SIGN_UP_ROUTE} >Sign Up</Link></li>
+                    <li><Link className='md:ml-8 text-xl md:my-0 my-7 text-white hover:text-secondary duration-500' to={SIGN_IN_ROUTE} >Sign In</Link></li>
+                    <li><Link className='md:ml-8 text-xl md:my-0 my-7 text-white hover:text-secondary duration-500' to={LOG_OUT} >Log Out</Link></li>
+                    <button type='submit' id='translation-icon'>
+                    <SiGoogletranslate className=' text-3xl md:ml-8 md:my-0 my-7 text-white hover:text-secondary duration-500' />
+                </button>
+                </ul>
+            </div>
+        </div>
     )
 }
-
 export default NavBar;
