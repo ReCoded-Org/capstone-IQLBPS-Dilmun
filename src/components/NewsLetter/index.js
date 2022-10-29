69e1234cd4ccf8cab6b542e18e4b7dce72bc6430
Omar Bassatni <omarbassatni@gmail.com>
2022-10-29 21:23:05 +0300

    deleted left and right margins

--- src/components/NewsLetter/index.js
+++ src/components/NewsLetter/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NewsLetter = () => {
   return (
     <section>
-      <div className='m-14 pt-14 pb-14 pr-10 pl-10 md:flex rounded-md shadow-md justify-around bg-primary'>
+      <div className='mt-14 pt-14 pb-14 pr-10 pl-10 md:flex rounded-md shadow-md justify-around bg-primary'>
         <div className='text-background flex flex-col md:flex-row justify-center items-center'>
           <img className='w-24 md:w-40  h-100' src="https://www.freepnglogos.com/uploads/blue-email-logo-png-4.png" alt="email" />
           <h1 className='text-[20px] sm:text-[40px] mb-3 sm:mb-0'>Subscribe to our <br /> Newsletter !</h1>
