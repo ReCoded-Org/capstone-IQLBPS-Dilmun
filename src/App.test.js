d9fd5d19867bc14d815ae2e0f763acc0d21774bc
Omar Bassatni <omarbassatni@gmail.com>
2022-10-29 11:35:36 +0300

    deleted other tests from app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-// import { Provider } from "react-redux";
-// import { store } from "./app/store";
-// import App from "./App";
 import NewsLetter from "./components/NewsLetter";
-
-
-// test("renders learn react link", () => {
-//   const { getByText } = render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   );
-
-//   expect(getByText(/learn/i)).toBeInTheDocument();
-// });
 
 
 it('render correctly', () => {
