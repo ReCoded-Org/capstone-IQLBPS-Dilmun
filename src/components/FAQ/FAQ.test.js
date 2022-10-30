f12139b0e86c0f978bcddf33cd07f4679582dcb6
Marwa Hakmi <marwahakmi@gmail.com>
2022-10-31 00:02:11 +0200

    test: added some tests to FAQ comp.

--- src/components/FAQ/FAQ.test.js
+++ src/components/FAQ/FAQ.test.js
@@ -1,18 +1,73 @@
 import { render, screen } from '@testing-library/react'
-
+import userEvent from '@testing-library/user-event'
 import FAQ from '.'
 
-describe("Testing FAQ component", () => {
-    render(<FAQ />)
+test('should have a title', () => {
+    render(<FAQ />);
 
-    it('should have a title', () => {
-        expect(screen.findByText("Frequently Asked Questions"))
+    const title = screen.getByRole('heading', {
+        name: /frequently asked questions/i
     })
+    expect(title).toBeTruthy();
+});
 
-    // it("should render an img with alt attribute", () => {
-    //     const testImage = document.querySelector("img");
-    //     expect(testImage.alt).toContain("frequently asked questions");
-    // })
+test('should have an image on the right of screen', () => {
+    render(<FAQ />);
 
+    const img = screen.getByRole('img', {
+        name: /frequently asked questions/i
+    })
+    expect(img).toBeTruthy();
+});
+
+test('should have a question', () => {
+    render(<FAQ />);
+
+    const question = screen.getByText(/Can I cancel my order?/i)
+    expect(question).toBeTruthy();
+})
+
+test('should have a answer', async () => {
+    render(<FAQ />);
+
+    const btn = screen.getByRole('button', {
+        name: /can i cancel my order/i
+    })
+    userEvent.click(btn)
+
+    const answer = await screen.findByText(/you can cancel your order/i)
+    expect(answer).toBeTruthy();
 
 })
+
+// test to check if bg color and text color changed on click.
+// test('should change bg color and text color on click', async () => {
+//     render(<FAQ />);
+//     const btn = screen.getByRole('button', {
+//         name: /can i cancel my order/i
+//     })
+//     userEvent.click(btn)
+//     expect(btn).toHaveStyle({
+//         backgroundColor: 'rgb(22, 66, 91, 1)',
+//         color: 'rgb(255, 255, 255, 1)'
+//     })
+// })
+
+test('should have a question', () => {
+    render(<FAQ />);
+
+    const question = screen.getByText(/What is the return policy?/i)
+    expect(question).toBeTruthy();
+})
+
+test('should have a answer', async () => {
+    render(<FAQ />);
+
+    const btn = screen.getByRole('button', {
+        name: /what is the return policy/i
+    })
+    userEvent.click(btn)
+
+    const answer = await screen.findByText(/There must be a deal between the customers and the sellers to determine how returning items should be and our staff is not responsible of it/i)
+    expect(answer).toBeTruthy();
+});
