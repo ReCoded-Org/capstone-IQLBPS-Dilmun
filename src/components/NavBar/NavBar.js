013ffb0751d462afe0e1127db951c9a7ee97d387
Oussama Masri <oussamamasri16120@gmail.com>
2022-10-30 18:07:06 +0200

    fixed some bug

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -20,11 +20,12 @@ function NavBar(){
 
     return(
      <nav>
-        <div className='md:flex bg-primary items-center justify-between md:px-8 px-10 shadow-md w-full fixed top-0 left-0'>
+        <div className='shadow-md w-full fixed top-0 left-0 bg-primary'>
+            <div className='md:flex bg-primary py-3 items-center justify-between md:px-8 px-10'>
                 <div >
                 <Link to={HOME_ROUTE}>
                 <img src='https://cdn.discordapp.com/attachments/1031834305703460906/1035627738440159303/Asset_23.png'
-                className='object-scale-down h-20 justify-self-center py-0 mt-2' alt='logo'/>
+                className='object-scale-down h-20 md:justify-self-center py-0' alt='logo'/>
                 </Link>
                 </div>
                 <button 
@@ -32,7 +33,7 @@ function NavBar(){
                 onClick={()=>setOpen(!open)}>  
                 {open ? <AiOutlineClose className='text-6xl text-secondary absolute right-8 top-7 cursor-pointer md:hidden'/> : <FaBars className='text-6xl text-secondary absolute right-8 top-7 cursor-pointer md:hidden'/>}
                 </button>
-                <ul className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-primary md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'}`}>
+                <ul className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-primary md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-500 ease-in ${open ? 'top-32':'top-[-490px]'}`}>
                     <li><Link className='md:ml-6 text-xl md:my-0 text-background hover:text-secondary duration-500' to={HOME_ROUTE} >Home</Link></li>
                     <li><Link className='md:ml-6 text-xl md:my-0 text-background hover:text-secondary duration-500' to={CONTACT_ROUTE} >Contact</Link></li>
                     <li><Link className='md:ml-6 text-xl md:my-0 text-background hover:text-secondary duration-500' to={ABOUT_ROUTE} >About</Link></li>
@@ -40,10 +41,11 @@ function NavBar(){
                     <li><Link className='md:ml-6 text-xl md:my-0 text-background hover:text-secondary duration-500' to={SIGN_UP_ROUTE} >SignUp</Link></li>
                     <li><Link className='md:ml-6 text-xl md:my-0 text-background hover:text-secondary duration-500' to={SIGN_IN_ROUTE} >SignIn</Link></li>
                     <li><Link className='md:ml-6 text-xl md:my-0 text-background hover:text-secondary duration-500' to={LOG_OUT} >LogOut</Link></li>
-                    <li><button type='submit'>
+                    <button type='submit'>
                     <SiGoogletranslate className=' text-3xl md:ml-6 md:my-0 text-background hover:text-secondary duration-500' />
-                </button></li>
+                </button>
                 </ul>
+            </div>
         </div>
     </nav>
     )
