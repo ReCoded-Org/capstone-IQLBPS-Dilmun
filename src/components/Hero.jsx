32d8d0f77bfe7a66470f9ffc706a1e0cb667d3a1
sozi <sozyarnadir@gmail.com>
2022-10-30 12:38:20 +0300

    Created the hero page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+
+function Hero () {
+  return (
+<div className='w-full h-[90vh] top[90px]'> 
+
+<div className='w-full h-[90%] flex flex-col justify-center items-center text-bg-black text-center'>
+<h1 className='text-5xl font-bold'>
+  Welcome
+</h1>
+
+<h1 className='text-xl py-4'>
+  <span className="text-blue-600">to</span>This website
+</h1>
+
+<p className='text-xl py-4'>Best offers and qualities</p>
+
+<button type="button" className="btn btn-blue">
+Get Started
+</button>
+
+
+</div>
+
+</div>
+
+  )}
+    
+  export default Hero;
