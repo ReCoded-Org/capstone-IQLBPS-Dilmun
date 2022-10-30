60cef5f2eae71d4a7d0c1d108678b587014e0fb7
Oussama Masri <67838526+OussamaMasri@users.noreply.github.com>
2022-10-30 20:30:54 +0200

    15 create navigation bar (#57)
    * Add: Added the nav logIn and LogOut (phase1)
    
    * Merge: merged the navbar logIn and LogOut in the same page
    
    * re-installed react-rouer-dom cuz it was playing games with me
    
    * navbar styling in progress
    
    * Update: updated the navbar the style has been fixed, its responsive, contain some weird bugs still processing (phase1)
    
    * Update : updated the navbar background color
    
    * update: styling adjustments
    
    * Update: changed the NavLink to Link
    
    * Add: added the first navbar test still processing (phase1)
    
    * added test file
    
    * Update: NavBar updated the logos are on the sides
    
    * snapshot test added
    
    * snapshot updated
    
    * Update: done some refactoring
    
    * importing navbar error fixed
    
    * Fix: fixed the NavBar
    
    * Fix: added the nav element to pass the CI error
    
    * Fix: Fixed a bug in the responsiveness and changed the color of the FaBars and OutLineClose
    
    * Fix: Fixed the spacing between each word
    
    * Fix: fixing some bugs phase1
    
    * snapshot updated
    
    * fixed styling of menu dropdown
    
    * did some styling adjustments
    
    * snapshot updated
    
    * fixed some bug
    
    * fixed some bugs
    
    * nav styling fixed
    
    * app.css file deleted
    
    * app.css importing removed from app.jsx
    
    * Fix: fixed some bugs
    
    * menu icon cented and snapshot updated
    
    * Fix: fixed some bugs
    
    * Fix: fixed the snapshot test
    
    * drop down items aligned in the center, snapshot updated
    
    * margin top added to translation icon
    
    Co-authored-by: Sarah Abou Dalal <sarahaboudalal@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
-import './App.css';
+import NavBar from './components/NavBar/NavBar'    
 import Features from './components/Features/Features';
 import NewsLetter from './components/NewsLetter';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Features />
       <NewsLetter />
     </div>
