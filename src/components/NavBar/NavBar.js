00d97ad8bf7f9a2f69cd8811fa2f9dd56f196abf
Oussama Masri <oussamamasri16120@gmail.com>
2022-10-30 19:42:19 +0200

    Fix: fixed some bugs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -19,7 +19,7 @@ function NavBar() {
 
   return (
     <nav className="shadow-md w-full fixed top-0 left-0 bg-primary">
-      <div className="md:flex bg-primary py-3 items-center justify-between md:px-8 px-10 max-h-24">
+      <div className="md:flex bg-primary py-2 items-center justify-between md:px-8 px-10 max-h-24">
         <div>
           <Link to={HOME_ROUTE}>
             <img
@@ -31,14 +31,14 @@ function NavBar() {
         </div>
         <button type="button" onClick={() => setOpen(!open)}>
           {open ? (
-            <AiOutlineClose className="text-6xl text-secondary absolute right-8 top-7 cursor-pointer md:hidden" />
+            <AiOutlineClose className="text-6xl text-secondary absolute right-8 top-6 cursor-pointer md:hidden" />
           ) : (
-            <FaBars className="text-6xl text-secondary absolute right-8 top-7 cursor-pointer md:hidden" />
+            <FaBars className="text-6xl text-secondary absolute right-8 top-6 cursor-pointer md:hidden" />
           )}
         </button>
         <ul
           className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-primary md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-500 ease-in ${
-            open ? 'top-32' : 'top-[-490px]'
+            open ? 'top-20' : 'top-[-490px]'
           }`}
         >
           <li>
