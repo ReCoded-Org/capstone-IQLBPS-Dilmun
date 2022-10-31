3d62972807fe34279356d2ea88a5cc92aff9c655
Maryam <marym.ihnj@gmail.com>
2022-10-31 19:43:43 +0300

    Centered Text and Button

--- src/components/DiffSection.js
+++ src/components/DiffSection.js
@@ -1,18 +1,19 @@
 import React from "react";
-import CoffeeShopCommercial from "../assets/CoffeeShopCommercial.mp4"
+import CoffeeShopCommercial from "../assets/CoffeeShopCommercial.mp4";
 import OverlapText from "./DiffSection/OverlapText"
-import Button from "./DiffSection/Button";
+
 
 function DiffSection(){
     return (
-        <div>
-            <OverlapText />
+        <div className="relative">
+            
             <div className="DiffVid relative flex justify-center pb-2 overflow-hidden">
                 <video src={CoffeeShopCommercial} autoPlay loop muted className="opacity-75 drop-shadow-2xl">
                 <track kind="captions"/>
                 </video>
+                <OverlapText />
             </div>
-            <Button />
+            
         </div>
     )
 }
--- src/components/DiffSection/OverlapText.js
+++ src/components/DiffSection/OverlapText.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Button from "./Button";
 
 function OverlapText() {
     return (
-        <div className="relative z-30 p-5 text-2xl text-black bg-blue-300 bg-opacity-50">
+    <div className ="absolute mt-60 my-10">
+        <div className="relative z-30 p-5 text-2xl text-black bg-opacity-20">
             <h2 className="text-black font-bold">Check this awesome video made by one of our satisfied customers!</h2>
         </div>
+        <Button />
+    </div>
     )
 }
 
