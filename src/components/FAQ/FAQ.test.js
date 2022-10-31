ff6551eb668c6c6331ebd177a28f92bfaaa6bdc7
Marwa Hakmi <marwahakmi@gmail.com>
2022-10-31 11:05:16 +0200

    test: add snapshot test for faq comp.

--- src/components/FAQ/FAQ.test.js
+++ src/components/FAQ/FAQ.test.js
@@ -1,6 +1,12 @@
+import renderer from 'react-test-renderer';
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import FAQ from '.'
+
+test('FAQ renders correctly', () => {
+    const tree = renderer.create(<FAQ />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
 
 test('should have a title', () => {
     render(<FAQ />);
@@ -39,19 +45,6 @@ test('should have a answer', async () => {
     expect(answer).toBeTruthy();
 
 })
-
-// test to check if bg color and text color changed on click.
-// test('should change bg color and text color on click', async () => {
-//     render(<FAQ />);
-//     const btn = screen.getByRole('button', {
-//         name: /can i cancel my order/i
-//     })
-//     userEvent.click(btn)
-//     expect(btn).toHaveStyle({
-//         backgroundColor: 'rgb(22, 66, 91, 1)',
-//         color: 'rgb(255, 255, 255, 1)'
-//     })
-// })
 
 test('should have a question', () => {
     render(<FAQ />);
