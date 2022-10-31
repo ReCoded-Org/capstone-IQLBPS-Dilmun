4ff547bc1bcb30baba174fe5a2885cf7f0565476
sozi <sozyarnadir@gmail.com>
2022-10-31 10:22:54 +0300

    Second draft

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,24 @@
 
 function Hero () {
-  return (
-<div className='w-full h-[90vh] top[90px]'> 
+return (
+<div className='backdrop-blur-md bg-white/30 w-full h-[100vh] top[90px] container text-background text-center'> 
+<div className='backdrop-blur-md w-full h-[100%] flex flex-col justify-center  text-bg-black'>
 
-<div className='w-full h-[90%] flex flex-col justify-center items-center text-bg-black text-center'>
-<h1 className='text-5xl font-bold'>
-  Welcome
+
+<h1 className=' text-background text-7xl font-bold text-center '>
+  Welcome to Dilumn 
 </h1>
-
-<h1 className='text-xl py-4'>
-  <span className="text-blue-600">to</span>This website
-</h1>
-
-<p className='text-xl py-4'>Best offers and qualities</p>
-
-<button type="button" className="btn btn-blue">
-Get Started
-</button>
-
+<div className=" ">
+<p className='w-100 text-background text-xl py-4 mx-80'>Through Dilumn You Can Get the Best Quality Products </p>
 
 </div>
+<div className="btn__container">
+<button type='button' className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full">
+  Get Started
+</button>
+</div>
 
+</div>
 </div>
 
   )}
