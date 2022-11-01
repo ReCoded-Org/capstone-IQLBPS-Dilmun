c891d5f2bae16b57055b667ae987cbe6ef77ec89
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-01 21:25:17 +0200

    almost done with the tests

--- src/components/Proof/__tests__/FeedbackCard.test.js
+++ src/components/Proof/__tests__/FeedbackCard.test.js
@@ -7,8 +7,20 @@ test('should render feedbackCard component', () => {
     expect(FeedbackCardElement).toBeInTheDocument();
 });
 
-// test('feedbackCard section h2 should contain (You’ll be in good company)', () => {
+// test('if feedbackcard content prop is passed, render the content', () => {
 //     render(<FeedbackCard />);
-//     const FeedbackCardTitle = screen.getByTestId('costumerslogo-title');
-//     expect(FeedbackCardTitle).toHaveTextContent('You’ll be in good company');
+//     const FeedbackContent = screen.getByTestId('feedback-card-content');
+//     expect(FeedbackContent).toHaveTextContent('I\'ve made multiple purchases and every single one has been great! The selection they offer is so varied I could spend hours looking through all the items.');
+// });
+
+// test('feature card should has a name', () => {
+//     render(<FeedbackCard />);
+//     const FeedbackName = screen.getByTestId('feedback-card-name');
+//     expect(FeedbackName).toBeTruthy();
+// });
+
+// test('feature card should has a title', () => {
+//     render(<FeedbackCard />);
+//     const FeedbackTitle = screen.getByTestId('feedback-card-title');
+//     expect(FeedbackTitle).toBeTruthy();
 // });
