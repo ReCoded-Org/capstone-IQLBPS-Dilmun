d00c4490375aa1d2eb44759d86b33ee3ab0d0be1
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-02 15:01:58 +0200

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,10 @@ import NavBar from './components/NavBar/NavBar'
 import Features from './components/Features/Features';
 import NewsLetter from './components/NewsLetter';
 import SearchBar from './components/SearchBar';
-import SignIn from './components/SignIn_SignUp/SignIn';
-import SignUp from './components/SignIn_SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
-      <SignUp />
       <NavBar />
       <SearchBar />
       <Features />
