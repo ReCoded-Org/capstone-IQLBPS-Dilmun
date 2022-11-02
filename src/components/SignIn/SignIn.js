4fb7b67ae742d1c12ef5eca5e6f16b77711fe248
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-02 21:49:43 +0200

    added extra styling to sign-in/up pages

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,7 +5,7 @@ function SignIn() {
     e.preventDefault();
   };
   return (
-    <div className="bg-background bg-signin-background bg-contain bg-no-repeat h-screen flex flex-col justify-center items-center w-screen">
+    <div className="bg-background bg-signin-background md:bg-contain bg-cover bg-no-repeat h-screen flex flex-col justify-center items-center w-screen">
       <h1 className="text-5xl font-bold mb-10 text-primary">SIGN IN</h1>
       <form onSubmit={handleSubmit} className="flex flex-col justufy-center">
         <label htmlFor="email">
@@ -14,7 +14,7 @@ function SignIn() {
             type="email"
             placeholder="Email"
             required
-            className="text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block"
+            className="sm:w-96 w-80 shadow-lg text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
           />
         </label>
         <label htmlFor="password">
@@ -23,24 +23,24 @@ function SignIn() {
             type="password"
             placeholder="Password"
             required
-            className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block"
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
           />
         </label>
         <button
           type="submit"
-          className="my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
+          className="sm:w-96 w-80 font-semibold shadow-lg my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
         >
           Sign In
         </button>
         <button
           type="button"
-          className="my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
+          className="sm:w-96 w-80 font-semibold shadow-lg my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
         >
           Sign In With Google
         </button>
         <button
           type="button"
-          className="my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
+          className="sm:w-96 w-80 font-semibold shadow-lg my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
         >
           Sign In With Facebook
         </button>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -6,7 +6,7 @@ function SignUp() {
   };
 
   return (
-    <div className="bg-background bg-signin-background bg-contain bg-no-repeat h-screen flex flex-col justify-center items-center w-screen">
+    <div className="bg-background bg-signin-background sm:bg-contain bg-cover bg-no-repeat h-screen flex flex-col justify-center items-center w-screen">
     <h1 className="text-5xl font-bold mb-10 text-primary ">SIGN UP</h1>
       <form onSubmit={handleSubmit} className="flex flex-col justufy-center">
         <label htmlFor="first-name">
@@ -15,7 +15,7 @@ function SignUp() {
             type="text"
             placeholder="First Name"
             required
-            className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block"
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
           />
         </label>
         <label htmlFor="last-name">
@@ -24,7 +24,7 @@ function SignUp() {
             type="text"
             placeholder="Last Name"
             required
-            className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block"
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
           />
         </label>
         <label htmlFor="email">
@@ -33,16 +33,16 @@ function SignUp() {
             type="email"
             placeholder="Email"
             required
-            className="text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block"
+            className="sm:w-96 w-80 shadow-lg text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
           />
         </label>
         <label htmlFor="password">
-          <span className="text-primary font-semibold">Password</span>
+          <span className="sm:w-96 w-80 shadow-lg text-primary font-semibold">Password</span>
           <input
             type="password"
             placeholder="Password"
             required
-            className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block"
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
           />
         </label>
         <label htmlFor="confirm-password">
@@ -51,24 +51,24 @@ function SignUp() {
             type="password"
             placeholder="Confirm Password"
             required
-            className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block"
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
           />
         </label>
         <button
           type="submit"
-          className="my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
+          className="sm:w-96 w-80 font-semibold shadow-lg my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
         >
           Sign Up
         </button>
         <button
           type="button"
-          className="my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
+          className="sm:w-96 w-80 font-semibold shadow-lg my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
         >
           Sign Up With Google
         </button>
         <button
           type="button"
-          className="my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
+          className="sm:w-96 w-80 font-semibold shadow-lg my-2 bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary duration-500"
         >
           Sign Up With Facebook
         </button>
