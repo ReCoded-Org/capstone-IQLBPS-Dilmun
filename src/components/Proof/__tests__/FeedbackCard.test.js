f0e20d50a977ceb378c79c32ddd2852205dc16c2
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-02 16:27:05 +0200

    almost done with the tests

--- src/components/Proof/__tests__/FeedbackCard.test.js
+++ src/components/Proof/__tests__/FeedbackCard.test.js
@@ -7,20 +7,10 @@ test('should render feedbackCard component', () => {
     expect(FeedbackCardElement).toBeInTheDocument();
 });
 
-// test('if feedbackcard content prop is passed, render the content', () => {
-//     render(<FeedbackCard />);
-//     const FeedbackContent = screen.getByTestId('feedback-card-content');
-//     expect(FeedbackContent).toHaveTextContent('I\'ve made multiple purchases and every single one has been great! The selection they offer is so varied I could spend hours looking through all the items.');
-// });
-
-// test('feature card should has a name', () => {
-//     render(<FeedbackCard />);
-//     const FeedbackName = screen.getByTestId('feedback-card-name');
-//     expect(FeedbackName).toBeTruthy();
-// });
-
-// test('feature card should has a title', () => {
-//     render(<FeedbackCard />);
-//     const FeedbackTitle = screen.getByTestId('feedback-card-title');
-//     expect(FeedbackTitle).toBeTruthy();
-// });
+const feedback = {
+      id: "feedback-1",
+      content:
+        "I've made multiple purchases and every single one has been great! The selection they offer is so varied I could spend hours looking through all the items.",
+      name: "Herman Jensen",
+      title: "Client",
+};
--- src/components/Proof/__tests__/Testimonials.test.js
+++ src/components/Proof/__tests__/Testimonials.test.js
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom";
 import { render, screen } from '@testing-library/react';
 import Testimonials from '../Testimonials';
 
@@ -19,8 +20,36 @@ test('testimonials section p should contain (Everything you need to know about o
     expect(TestimonialsParagraph).toHaveTextContent('Everything you need to know about our business and products anywhere on the planet.');
 });
 
-// test('testimonials section should contain 3 feature cards', () => {
-//     render(<Testimonials />);
-//     const TestimonialsCard = screen.getAllByTestId('testimonials-card');
-//     expect(TestimonialsCard).toHaveLength(3);
-// });
+const feedback = [
+    {
+      id: "feedback-1",
+      content:
+        "I've made multiple purchases and every single one has been great! The selection they offer is so varied I could spend hours looking through all the items.",
+      name: "Herman Jensen",
+      title: "Client",
+    },
+    {
+      id: "feedback-2",
+      content:
+        "Every purchase felt secure and all items were received in a timely manner. What a great place to showcase businesses goods!",
+      name: "Steve Mark",
+      title: "Client",
+    },
+    {
+      id: "feedback-3",
+      content:
+        "I found that Dilmun was very professional and delivered a quality product in an efficient time frame. I would consider shopping with them again.",
+      name: "Kenn Gallagher",
+      title: "Client",
+    }
+  ];
+  
+  test("renders a <div> element", () => {
+    const { container } = render(<Testimonials cards={feedback}/>);
+    expect(container.querySelector("div")).toBeInTheDocument();
+  });
+  
+  test("renders a card component for each post passed as a prop", () => {
+    const { container } = render(<Testimonials cards={feedback}/>);
+    expect(container.querySelector("div").children).toHaveLength(2);
+  });
