c7df0ab656294ec3ef3255a50d9a02b5a0f5bd75
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-02 16:52:00 +0200

    change color of text input

--- src/components/SignIn_SignUp/SignIn.js
+++ src/components/SignIn_SignUp/SignIn.js
@@ -10,11 +10,11 @@ function SignIn() {
       <form onSubmit={handleSubmit} className="flex flex-col justufy-center">
         <label htmlFor="email" >
           <span className='text-primary font-semibold'>Email</span>
-          <input type="email" placeholder="Email" required className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
+          <input type="email" placeholder="Email" required className="text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
         </label>
         <label htmlFor="password">
           <span className='text-primary font-semibold'>Password</span>
-          <input type="password" placeholder="Password" required className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
+          <input type="password" placeholder="Password" required className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
         </label>
         <button type='submit' className='bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary'>Sign In</button>
       </form>
