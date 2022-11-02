16d05f47951570d2d11694f23292ebec60865cec
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-02 16:46:44 +0200

    almost done with the tests

--- src/components/Proof/__tests__/FeedbackCard.test.js
+++ src/components/Proof/__tests__/FeedbackCard.test.js
@@ -7,10 +7,3 @@ test('should render feedbackCard component', () => {
     expect(FeedbackCardElement).toBeInTheDocument();
 });
 
-const feedback = {
-      id: "feedback-1",
-      content:
-        "I've made multiple purchases and every single one has been great! The selection they offer is so varied I could spend hours looking through all the items.",
-      name: "Herman Jensen",
-      title: "Client",
-};
