afe47dadc27fa6962029d88c3693e0f6fa959f85
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-02 16:42:21 +0200

    sign in components styled (stage 1)

--- src/components/SignIn_SignUp/SignIn.js
+++ src/components/SignIn_SignUp/SignIn.js
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
-function SignIn(){
-    return (
-        <div className="bg-background h-screen flex justify-center items-center">
-            <div>
-                <form action="" >
-                    <div>
-                        <p>LOGIN</p>
-                    </div>
-                    <div>
-                        <p>Email</p>
-                        <input type="email" placeholder='Email' required />
-                    </div>
-                    <div>
-                        <p>Password</p>
-                        <input type="password" placeholder='Password' required />
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+function SignIn() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="bg-background h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold my-4 text-primary">SIGN IN</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col justufy-center">
+        <label htmlFor="email" >
+          <span className='text-primary font-semibold'>Email</span>
+          <input type="email" placeholder="Email" required className="rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
+        </label>
+        <label htmlFor="password">
+          <span className='text-primary font-semibold'>Password</span>
+          <input type="password" placeholder="Password" required className="rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
+        </label>
+        <button type='submit' className='bg-primary text-background py-2 rounded-md'>Sign In</button>
+      </form>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
