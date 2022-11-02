e0959f81fd6aef6ef4f4ee728e1baade2d76a04f
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-02 19:38:58 +0200

    made the code structure as kutay wants

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import SignIn from '../components/SignIn/SignIn'
+import React from 'react';
+import SignIn from '../components/SignIn/SignIn';
 
 export default function SignInPage() {
-    return(
-        <div>
-        <SignIn/>
-        </div>
-    )
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
 }
--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import SignUp from "../components/SignUp/SignUp";
+import React from 'react';
+import SignUp from '../components/SignUp/SignUp';
 
-export default function SignUpPage(){
-    return(
-        <div>
-        <SignUp/>
-        </div>
-    )
+export default function SignUpPage() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
 }
