5ede9193a8ca23df35d5a185c29778646f69f226
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-02 15:49:06 +0200

    signinpage setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+// import {Routes, Route} from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar';
 import Features from './components/Features/Features';
 import NewsLetter from './components/NewsLetter';
 import SearchBar from './components/SearchBar';
-import SignIn from './components/SignIn_SignUp/SignIn';
+import SignInPage from './Pages/SignInPage'
+
+// import {
+//   SIGN_IN_ROUTE,
+// } from './route';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
-      <NavBar />
+    <NavBar />
+    <SignInPage/>
       <SearchBar />
       <Features />
       <NewsLetter />
---
+++ src/Pages/SignInPage.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import SignIn from '../components/SignIn_SignUp/SignIn'
+
+export default function SignInPage() {
+    return(
+        <div>
+        <SignIn/>
+        </div>
+    )
+}
