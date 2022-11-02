c38e66caee2fea0eef00bba2e27e5f9eb5780eca
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-02 16:50:33 +0200

    added on hover and on focus styling

--- src/components/SignIn_SignUp/SignIn.js
+++ src/components/SignIn_SignUp/SignIn.js
@@ -10,13 +10,13 @@ function SignIn() {
       <form onSubmit={handleSubmit} className="flex flex-col justufy-center">
         <label htmlFor="email" >
           <span className='text-primary font-semibold'>Email</span>
-          <input type="email" placeholder="Email" required className="rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
+          <input type="email" placeholder="Email" required className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
         </label>
         <label htmlFor="password">
           <span className='text-primary font-semibold'>Password</span>
-          <input type="password" placeholder="Password" required className="rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
+          <input type="password" placeholder="Password" required className="focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block" />
         </label>
-        <button type='submit' className='bg-primary text-background py-2 rounded-md'>Sign In</button>
+        <button type='submit' className='bg-primary text-background py-2 rounded-md hover:bg-tertiary hover:text-primary'>Sign In</button>
       </form>
     </div>
   );
