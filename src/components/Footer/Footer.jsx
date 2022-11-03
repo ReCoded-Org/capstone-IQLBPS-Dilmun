130660eb86f21c27cbddaeaf00761e5fa82484a0
karzan <karzanluqman99@gmail.com>
2022-11-03 03:26:35 +0300

    Updating the Branch

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <Link className="text-background hover:text-secondary" to={HOME_ROUTE}>Dilmun</Link>. All Rights Reserved.
             </span>
-        </div >
+        </div>
 
 
 
