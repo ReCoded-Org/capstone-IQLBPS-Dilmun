bf6f050b4f24b7973f5a373e24cbf76eecebfc80
sozi <sozyarnadir@gmail.com>
2022-11-03 21:11:54 +0300

    Minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ function Hero() {
       <div className='backdrop-blur-md w-full h-full  flex flex-col justify-center text-center'>
         <div className=" text-bg-black sm:mx-0 mx-4">
           <h1 className=' text-background text-7xl font-bold  '>
-            Welcome to Dilumn
+            Welcome to Dilmun
           </h1>
-          <p className='text-background text-xl my-4'>Through Dilumn You Can Get the Best Quality Products </p>
+          <p className='text-background text-xl my-4'>Your Way to Get Quality Products!</p>
           <div className="btn__container">
             <button type='button' className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full">
-              Get Started
+            Sign Up
             </button>
           </div>
         </div>
