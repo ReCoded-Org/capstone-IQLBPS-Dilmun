771afc20e328fe355a03a38e15b67896b51da87e
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-03 23:08:45 +0200

    validation added

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,25 +5,25 @@ function SignIn() {
     e.preventDefault();
   };
   return (
-    <div className="bg-background bg-signin-background md:bg-contain bg-cover bg-no-repeat h-screen flex flex-col justify-center items-center w-screen">
+    <div className="bg-background bg-signin-background md:bg-contain bg-cover bg-no-repeat flex flex-col justify-center items-center w-screen">
       <h1 className="text-5xl font-bold mb-10 text-primary pt-9">SIGN IN</h1>
       <form onSubmit={handleSubmit} className="flex flex-col justufy-center">
-        <label htmlFor="email">
+        <label htmlFor="email" className='mt-3'>
           <span className="text-primary font-semibold">Email</span>
           <input
             type="email"
             placeholder="Email"
             required
-            className="sm:w-96 w-80 shadow-lg text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500 "
+            className="sm:w-96 w-80 shadow-lg text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 mt-1 block duration-500 "
           />
         </label>
-        <label htmlFor="password">
+        <label htmlFor="password" className='mt-3'>
           <span className="text-primary font-semibold">Password</span>
           <input
             type="password"
             placeholder="Password"
             required
-            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 mt-1 block duration-500"
           />
         </label>
         <button
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,86 +1,114 @@
 import React from 'react';
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 const schema = yup.object().shape({
-  firstName: yup.string().required("Please insert your First Name"),
-  lastName: yup.string().required("Please insert your Last Name"),
-  email: yup.string().email().required("Please insert your Email"),
-  password: yup.string().min(4).max(15).required("Please insert your Password"),
-  confirmPassword: yup.string().oneOf([yup.ref("password"), null]),
+  firstName: yup.string().required('Please insert your First Name'),
+  lastName: yup.string().required('Please insert your Last Name'),
+  email: yup
+    .string()
+    .email('Should be a valid email')
+    .required('Please insert your Email'),
+  password: yup
+    .string()
+    .min(6, 'Should be at least 6 characters')
+    .max(15, "Shouldn't be more than 15 characters")
+    .required('Please insert your Password'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null])
+    .required('Please Confirm Password'),
 });
 
 function SignUp() {
-
-  const { register, handleSubmit, errors } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="bg-background bg-signin-background sm:bg-contain bg-cover bg-no-repeat h-screen flex flex-col justify-center items-center w-screen">
-    <h1 className="text-5xl font-bold mb-10 text-primary pt-9 ">SIGN UP</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col justufy-center">
-        <label htmlFor="first-name">
+    <div className="bg-background bg-signin-background sm:bg-contain bg-cover bg-no-repeat flex flex-col justify-center items-center w-screen">
+      <h1 className="text-5xl font-bold mb-10 text-primary pt-9 ">SIGN UP</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justufy-center"
+      >
+        <label htmlFor="firstName" className='mt-3'>
           <span className="text-primary font-semibold">First Name</span>
           <input
             type="text"
-            name='firstName'
             placeholder="First Name"
-            ref={register}
-            required
-            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
+            name="firstName"
+            {...register('firstName')}
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 mt-1 block duration-500"
           />
-          <p> {errors.firstName?.message} </p>
+          <p className="text-red-800 font-semibold">
+            {errors?.firstName?.message}
+          </p>
         </label>
-        <label htmlFor="last-name">
+        <label htmlFor="lastName" className='mt-3'>
           <span className="text-primary font-semibold">Last Name</span>
           <input
             type="text"
-            name='lastName'
+            name="lastName"
             placeholder="Last Name"
-            ref={register}
-            required
-            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
+            {...register('lastName')}
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 mt-1 block duration-500"
           />
-          <p> {errors.lastName?.message} </p>
+          <p className="text-red-800 font-semibold">
+            {' '}
+            {errors?.lastName?.message}{' '}
+          </p>
         </label>
-        <label htmlFor="email">
+        <label htmlFor="email" className='mt-3'>
           <span className="text-primary font-semibold">Email</span>
           <input
             type="email"
-            name='email'
+            name="email"
             placeholder="Email"
-            ref={register}
-            required
-            className="sm:w-96 w-80 shadow-lg text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
+            {...register('email')}
+            className="sm:w-96 w-80 shadow-lg text-primary focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 mt-1 block duration-500"
           />
-          <p> {errors.email?.message} </p>
+          <p className="text-red-800 font-semibold">
+            {' '}
+            {errors?.email?.message}{' '}
+          </p>
         </label>
-        <label htmlFor="password">
+        <label htmlFor="password" className='mt-3'>
           <span className="text-primary font-semibold">Password</span>
           <input
             type="password"
-            name='password'
+            name="password"
             placeholder="Password"
-            ref={register}
-            required
-            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
+            {...register('password')}
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 mt-1 block duration-500"
           />
-          <p> {errors.password?.message} </p>
+          <p className="text-red-800 font-semibold">
+            {' '}
+            {errors?.password?.message}{' '}
+          </p>
         </label>
-        <label htmlFor="confirm-password">
+        <label htmlFor="confirmPassword" className='mt-3'>
           <span className="text-primary font-semibold">Confirm Password</span>
           <input
             type="password"
-            name='confirmPassword'
+            name="confirmPassword"
             placeholder="Confirm Password"
-            ref={register}
-            required
-            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 my-3 block duration-500"
+            {...register('confirmPassword')}
+            className="sm:w-96 w-80 shadow-lg focus:outline-none focus:tertiary focus:ring-1 focus:ring-tertiary text-primary rounded-md placeholder:italic placeholder:text-tertiary px-3 py-1 mt-1 block duration-500"
           />
-          <p> {errors.confirmPassword && "Passwords Should Match!"} </p>
+          <p className="text-red-800 font-semibold mt-1 mb-3">
+            {' '}
+            {errors?.confirmPassword?.message && 'Passwords should match'}{' '}
+          </p>
         </label>
         <button
           type="submit"
