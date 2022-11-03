e23866215beda88737f288ee9d02d04e5ffa75d9
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-03 19:29:17 +0200

    bug fixed

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import { yupResolver } from "yup"
 // import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
