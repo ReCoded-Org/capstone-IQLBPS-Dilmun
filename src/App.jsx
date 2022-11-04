5490b61061777b1d8eb83e5beb82f36e9d64aaef
dania <daniadeyar@gmail.com>
2022-11-04 23:19:44 +0300

    create router for home & about & not found pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import NavBar from './components/NavBar/NavBar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
 import Features from './components/Features/Features';
 import NewsLetter from './components/NewsLetter';
 import Footer from './components/Footer/Footer';
 import SearchBar from './components/SearchBar';
+import AboutUs from './components/AboutUs/AboutUs';
 
-function App() {
+const Home = () => {
   return (
     <div className="App">
-
-
-
       <NavBar />
       <SearchBar />
       <Features />
@@ -18,6 +17,57 @@ function App() {
       <Footer />
     </div>
   );
+};
+
+const AboutUsRoute = () => {
+  return (
+    <div>
+      <NavBar />
+      <AboutUs />
+      <Footer />
+    </div>
+  );
+};
+
+
+const NotFound = () => {
+  return (
+    <div>
+          <NavBar />
+
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+              fontSize: '2rem',
+            }}
+          >
+            404 Not Found
+          </div>
+          <Footer />
+        </div>
+  )
+}
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/about',
+      element: <AboutUsRoute />,
+    },
+    {
+      path: '*',
+      element: <NotFound/>
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
