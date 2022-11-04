9d20b0f2cdadf1b437093c51164c04256ae79ac5
dania <daniadeyar@gmail.com>
2022-11-04 23:22:28 +0300

    creating about page & member's card

---
+++ src/components/AboutUs/MemberCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+
+const MemberCard = ({ member }) => {
+  return (
+    <div className="max-w-sm rounded-[20px] overflow-hidden shadow-lg">
+      <img className="w-full" src={member.img} alt="Sunset in the mountains" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{member.name}</div>
+        <p className="text-gray-700 text-base">{member.title}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        {member.links.map((link) => {
+          return (
+            <a
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              href={link.url}
+            >
+              <div className="flex space-x-2 items-center">
+                <div className='mr-1'>
+                {link.icon == 'linkedIn' ? (
+                  <AiFillLinkedin size={20} />
+                ) : (
+                  <AiFillGithub size={20} />
+                )}
+                </div>
+
+                {link.name}
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MemberCard;
+
+
+
