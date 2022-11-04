a0eee01cce6587569f49dea6077004fd25ed116e
Hiba <eng.lilil92@gmail.com>
2022-11-04 15:08:37 +0300

    requirments fixed

--- src/components/DiffSection.js
+++ src/components/DiffSection.js
@@ -6,12 +6,13 @@ import OverlapText from "./DiffSection/OverlapText"
 
 function DiffSection(){
     return (
-            <div className="rounded-md shadow-md bg-primary">
-                
-                <video src={Dilmun} autoPlay loop muted className=" opacity-50 drop-shadow-2xl rounded-md w-full h-full">
+            <div className="rounded-md shadow-md bg-primary w-full h-96 relative">
+                <video src={Dilmun} autoPlay loop muted className="w-full h-full object-cover opacity-60 drop-shadow-2xl rounded-md absolute">
                 <track kind="captions"/>
-                </video> 
-            <OverlapText />
+                </video>
+                <div  className=" relative mt-14 pt-24 lg:pt-18">
+                <OverlapText />
+                </div> 
             </div>
     )
 }
--- src/components/DiffSection/OverlapText.js
+++ src/components/DiffSection/OverlapText.js
@@ -3,8 +3,8 @@ import React from "react";
 function OverlapText() {
     return (
         <div className=" flex flex-col items-center justify-center text-black bg-opacity-20 pl-10 pr-10 ">
-            <h2 className=" text-white text-[20px] sm:text-[40px] ">Check this video made by one of our satisfied customers!</h2>
-            <button type="button" className="bg-tertiary font-semibold p-2 m-2 hover:bg-blue-700 text-white font-semibold rounded-lg w-full sm:w-auto ">
+            <h2 className=" text-white text-[20px] sm:text-[40px] ">Check out this video made by one of our satisfied customers!</h2>
+            <button type="button" className="bg-tertiary font-semibold p-2 m-2 hover:shadow-lg text-white font-semibold rounded-lg w-full sm:w-auto ">
                 Watch Full Video
             </button>
         </div>
