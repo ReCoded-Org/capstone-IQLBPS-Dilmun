ffea54c1737de74a2b291d1cb841a35bdcbe23d8
Omar Bassatni <omarbassatni@gmail.com>
2022-11-04 09:51:40 +0200

    added new category span

--- src/pages/ItemDetailsPage.js
+++ src/pages/ItemDetailsPage.js
@@ -8,7 +8,10 @@ const ItemDetailsPage = () => {
                 <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80" alt="headphones" />
             </div>
             <div className=' max-w-md ml-auto mr-auto flex flex-col gap-12 rounded pt-5 px-5 bg-primary '>
-                <h2 className='font-bold text-3xl '>Headphones</h2>
+                <div>
+                    <h2 className='font-bold text-3xl '>Headphones</h2>
+                    <span>Electronics</span>
+                </div>
                 <span>Price: $60</span>
                 <div>
                     <span className='font-bold text-xl'>Description:</span>
