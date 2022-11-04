69bb3cf9fa1099d0dd3397c41ce09ba4800999a0
Hiba <32968546+haboosh92@users.noreply.github.com>
2022-11-04 16:21:09 +0300

    Video Background (#56)
    * Video Background
    
    * Test added
    
    * Test edited
    
    * Added OverlapText component
    
    * Centered Text and Button
    
    * Video styling updated
    
    * test & responsive update
    
    * responsive issue fixed
    
    * requirments fixed
    
    * done
    
    Co-authored-by: Kutay Kağan Özen <kutay.kagan.ozenn@gmail.com>
    Co-authored-by: Maryam <marym.ihnj@gmail.com>

---
+++ src/components/DiffSection.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Dilmun from "../assets/Dilmun.mp4";
+import OverlapText from "./DiffSection/OverlapText"
+
+
+function DiffSection(){
+    return (
+            <div className=" rounded-md shadow-md bg-primary w-full h-96 relative">
+                <video src={Dilmun} autoPlay loop muted className="w-full h-full object-cover opacity-60 drop-shadow-2xl rounded-md absolute">
+                <track kind="captions"/>
+                </video>
+                <div  className=" relative mt-14 pt-14 lg:pt-12 ">
+                <OverlapText />
+                </div> 
+            </div>
+    )
+}
+export default DiffSection ;
