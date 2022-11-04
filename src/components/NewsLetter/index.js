2fa476c010a2d5a29ac46b3f32176063fee0c901
Omar Bassatni <omarbassatni@gmail.com>
2022-11-04 18:05:29 +0200

    Used Camel casing for custom-components folder

--- src/components/NewsLetter/index.js
+++ src/components/NewsLetter/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import CustomButton from '../customcomponents/CustomButton'
+import CustomButton from '../CustomComponents/CustomButton'
 
 const NewsLetter = () => {
   return (
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,7 @@ import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 // Custom Components
-import CustomButton from '../customcomponents/CustomButton'
+import CustomButton from '../CustomComponents/CustomButton'
 
 
 // just for the demo
