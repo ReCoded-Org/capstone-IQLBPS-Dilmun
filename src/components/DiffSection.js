de882b0dd1e05aafca8bd6237f2cdab9500845de
Hiba <eng.lilil92@gmail.com>
2022-11-04 16:11:26 +0300

    done

--- src/components/DiffSection.js
+++ src/components/DiffSection.js
@@ -1,16 +1,15 @@
 import React from "react";
- // import { player } from "video-react"
 import Dilmun from "../assets/Dilmun.mp4";
 import OverlapText from "./DiffSection/OverlapText"
 
 
 function DiffSection(){
     return (
-            <div className="rounded-md shadow-md bg-primary w-full h-96 relative">
+            <div className=" rounded-md shadow-md bg-primary w-full h-96 relative">
                 <video src={Dilmun} autoPlay loop muted className="w-full h-full object-cover opacity-60 drop-shadow-2xl rounded-md absolute">
                 <track kind="captions"/>
                 </video>
-                <div  className=" relative mt-14 pt-24 lg:pt-18">
+                <div  className=" relative mt-14 pt-14 lg:pt-12 ">
                 <OverlapText />
                 </div> 
             </div>
--- src/components/DiffSection/DiffSection.test.js
+++ src/components/DiffSection/DiffSection.test.js
@@ -1,25 +1,15 @@
 import React from "react";
 import OverlapText from "./OverlapText";
-import Button from "./Button";
 import DiffSection from "../DiffSection";
 import renderer from 'react-test-renderer'
-//import { renderer } from "@testing-library/react";
+
 
 test('DiffSection renders without crashing ', () => {
     const tree = renderer.create(
     <DiffSection>
-      <OverlapText>
-        <Button />
-      </OverlapText>
+      <OverlapText />
     </DiffSection>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   
   });
-
-//const Diff = require("../DiffSection");
-// test("DiffSection renders without crashing " , () => {
-//     const  = render(<DiffSection /> );
-//     expect(comp).toBeTruthy();
-//     expect(comp).toMatchSnapshot;
-// })
