6c1a752a0608d6adc1cbc0d7772e38c447027255
dania <daniadeyar@gmail.com>
2022-11-04 23:19:11 +0300

    Removing broswer router from index.js

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './i18n'
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
@@ -13,9 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider> 
   </React.StrictMode>
 );
