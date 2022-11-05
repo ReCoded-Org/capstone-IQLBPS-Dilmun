cd894c25dd360988a4c905158082e5f415e8714e
Hiba <eng.lilil92@gmail.com>
2022-11-05 17:08:58 +0300

    Button functionality added & full video deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-
-import NavBar from './components/NavBar/NavBar'
+import NavBar from './components/NavBar/NavBar'    
 import Features from './components/Features/Features';
 import DiffSection from './components/DiffSection';
 import NewsLetter from './components/NewsLetter';
-import SearchBar from './components/SearchBar/SearchBar';
-
 
 function App() {
   return (
     <div className="App"> 
       <NavBar />
-      <SearchBar />
       <Features />
       <DiffSection />
       <NewsLetter />
