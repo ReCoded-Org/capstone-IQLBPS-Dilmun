c0efafa1aabe69be24bef018c4b77287f2ca2bcf
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-05 17:06:37 +0200

    aligned in the center

--- src/Pages/SignInPage/SignInPage.jsx
+++ src/Pages/SignInPage/SignInPage.jsx
@@ -3,7 +3,7 @@ import SignIn from '../../components/SignIn/SignIn';
 
 export default function SignInPage() {
   return (
-    <div className="bg-background bg-signin-background bg-cover bg-no-repeat w-full h-screen" data-testid='sign-in-page'>
+    <div data-testid='sign-in-page'>
       <SignIn />
     </div>
   );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -37,7 +37,7 @@ function SignUp() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center" data-testid='sign-up'>
+    <div className="bg-background bg-signin-background bg-cover bg-no-repeat w-full min-h-[100vh] h-full flex flex-col justify-center items-center content-center" data-testid='sign-up'>
       <h1 className="text-5xl font-bold mb-10 text-primary pt-9">SIGN UP</h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
