f7e53b329fd27fa1499566da8bdc48cd30168f3d
Sozy <99980873+sozinadr@users.noreply.github.com>
2022-11-05 17:01:17 +0300

    8 create hero section of the homepage (#74)
    * .
    
    * .
    
    * Created the hero page
    
    * Second draft
    
    * Fixed width issue
    
    * Fixed responsiveness
    
    * Minor changes
    
    * Organizing folders
    
    * Creating hero.test file
    
    * Test files added
    
    * Ready for pull request
    
    * Solve merge conflicts
    
    Co-authored-by: Omar Bassatni <omarbassatni@gmail.com>

---
+++ src/components/HeroSection/Hero.jsx
@@ -0,0 +1,25 @@
+
+function Hero() {
+  return (
+    <div className='backdrop-blur-md bg-white/30 h-[100vh] cont text-center text-background'>
+      <div className='backdrop-blur-md w-full h-full  flex flex-col justify-center text-center'>
+        <div className=" text-bg-black sm:mx-0 mx-4">
+          <h1 className=' text-background text-7xl font-bold  '>
+            Welcome to Dilmun
+          </h1>
+          <p className='text-background text-xl my-4'>Your Way to Get Quality Products!</p>
+          <div className="btn__container">
+            <button type='button' className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full">
+            Sign Up
+            </button>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Hero;
