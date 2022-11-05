0cf4367f70fa0dae750a264983b2884afd0da899
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-05 16:38:55 +0200

    final height fix

--- src/Pages/SignUpPage/SignUpPage.jsx
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -3,7 +3,7 @@ import SignUp from '../../components/SignUp/SignUp';
 
 export default function SignUpPage() {
   return (
-    <div className="bg-background bg-signin-background bg-cover bg-no-repeat w-full h-[100-vh]" data-testid='sign-up-page'>
+    <div className="bg-background bg-signin-background bg-cover bg-no-repeat w-full min-h-[100vh] h-full" data-testid='sign-up-page'>
       <SignUp />
     </div>
   );
