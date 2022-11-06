c1ff72dac9f8ff415f921833343405418add4369
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-06 20:58:00 +0200

    hero bg configured with tailwind not css

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,25 +1,26 @@
-
 function Hero() {
   return (
-    <div className='backdrop-blur-md bg-white/30 md:h-[100vh] h-[85vh] cont text-center text-background duration-200 bg-cover'>
-      <div className='backdrop-blur-md w-full h-full  flex flex-col justify-center text-center'>
+    <div className="backdrop-blur-md bg-white/30 h-[100vh] bg-hero-bg bg-no-repeat text-center text-background bg-cover">
+      <div className="backdrop-blur-md w-full h-full  flex flex-col justify-center text-center">
         <div className=" text-bg-black md:mx-0 mx-4">
-          <h1 className=' text-background text-7xl font-bold  '>
+          <h1 className=" text-background text-7xl font-bold  ">
             Welcome to Dilmun
           </h1>
-          <p className='text-background text-xl my-4'>Your Way to Get Quality Products!</p>
+          <p className="text-background text-xl my-4">
+            Your Way to Get Quality Products!
+          </p>
           <div className="btn__container">
-            <button type='button' className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full">
-            Sign Up
+            <button
+              type="button"
+              className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full"
+            >
+              Sign Up
             </button>
           </div>
         </div>
-
-
       </div>
     </div>
-
-  )
+  );
 }
 
 export default Hero;
