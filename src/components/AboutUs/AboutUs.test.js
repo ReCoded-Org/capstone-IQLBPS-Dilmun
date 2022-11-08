7f25cbf57ff5552ac984c24edbc11edc904323f5
karzan <karzanluqman99@gmail.com>
2022-11-09 01:53:43 +0300

    snapshop test

---
+++ src/components/AboutUs/AboutUs.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import renderer from "react-test-renderer";
+// import { BrowserRouter as Router } from "react-router-dom"
+import AboutUS from "./AboutUs";
+import MemberCard from "./MemberCard";
+import team from './team.json';
+test("redering AboutUS", () => {
+    const tree = renderer.create(
+        <AboutUS />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+it('should render cards', () => {
+    // const books = ['Harry Potter', 'The Lord of the Rings']
+    const elem = renderer.create(
+
+        <AboutUS><MemberCard /></AboutUS>
+    ).toJSON();
+    expect(elem).toMatchSnapshot();
+});
+
