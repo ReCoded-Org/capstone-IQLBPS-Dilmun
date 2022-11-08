16b29e50c0e9e649543e2f41f27fec0e51bf3ad2
Asaad AbuGhazaleh <asaad.ag.98@gmail.com>
2022-11-08 18:29:05 +0200

    Implemented NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar'
 import SignInPage from './Pages/SignInPage/SignInPage';
 import SignUpPage from './Pages/SignUpPage/SignUpPage';
+import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
 import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
 
     </div>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NotFound.css';
+
+
+function NotFound() {
+
+  return (
+    <div className="mainbox">
+      <div className="err">404</div>
+
+      <div className="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?</div>
+    </div>
+  );
+}
+
+export default NotFound;
