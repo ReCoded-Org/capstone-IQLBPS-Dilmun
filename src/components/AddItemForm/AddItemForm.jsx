ef1f6eb4c9e46d47d820b623f7e654862fc78320
Marwa Hakmi <marwahakmi@gmail.com>
2022-11-08 11:42:00 +0200

    feat: creating Submit button comp. added loading dots animation when dealing with db

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -1,7 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import { Input, TextArea } from '../Forms';
+import { Input, TextArea, SubmitButton } from '../Forms';
 
 const schema = yup.object().shape({
   title: yup.string().required('Please insert your Item Name.'),
@@ -209,12 +209,7 @@ export default function AddItemForm() {
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Save
-                  </button>
+                  <SubmitButton buttonText="Add New Item" />
                 </div>
               </div>
             </form>
--- src/components/Forms/SubmitButton.jsx
+++ src/components/Forms/SubmitButton.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { LoadingDots } from '../animations';
 
-function SubmitButton() {
-  return <div />;
+const classNames = (...classes) => {
+  return classes.filter(Boolean).join(' ');
+};
+
+function SubmitButton({
+  buttonText,
+  disabled,
+  loading,
+  customClasses,
+  ...props
+}) {
+  return (
+    <button
+      type="submit"
+      disabled={disabled}
+      className={classNames(
+        'shadow-lg justify-center py-2 px-4 border border-secondary rounded-md text-sm font-medium text-white bg-secondary hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary',
+        disabled
+          ? 'opacity-50 cursor-not-allowed justify-center py-2 px-4 border border-green-200 rounded-md  text-sm font-medium text-white bg-green-200 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-200'
+          : '',
+        customClasses
+      )}
+      {...props}
+    >
+      {loading ? <LoadingDots /> : `${buttonText}`}
+    </button>
+  );
 }
 
 export default SubmitButton;
