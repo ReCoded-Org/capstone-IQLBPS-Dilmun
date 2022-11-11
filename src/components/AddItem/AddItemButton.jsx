27dc96f478f3dcfd00be4a787f7987942ba9923c
sozi <sozyarnadir@gmail.com>
2022-11-11 23:59:23 +0300

    .

--- src/components/AddItem/AddItemButton.jsx
+++ src/components/AddItem/AddItemButton.jsx
@@ -3,7 +3,7 @@ function addItem() {
   return ( 
 <div>
 <div className="btn__container">
-            <button type='button' className="">
+            <button type='button' className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full">  
             Add Item
             </button>
           </div>
