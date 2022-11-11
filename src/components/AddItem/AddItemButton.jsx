387cf4738dba4d43f6fc65ce8830813857332790
sozi <sozyarnadir@gmail.com>
2022-11-11 23:50:29 +0300

    add item button

---
+++ src/components/AddItem/AddItemButton.jsx
@@ -0,0 +1,14 @@
+
+function addItem() {
+  return ( 
+<div>
+<div className="btn__container">
+            <button type='button' className="">
+            Add Item
+            </button>
+          </div>
+</div>
+  )
+}
+
+export default addItem;
