81941fe19d9aa39c4077a6723516aa30bc11850b
dania <daniadeyar@gmail.com>
2022-11-12 19:02:00 +0300

    Added aboutUs to page's dir

---
+++ src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutUs from '../../components/AboutUs/AboutUs';
+
+const AboutUsPage = () => {
+  return (
+    <div data-testid="AboutUs-page">
+      <AboutUs />
+    </div>
+  );
+};
+
+export default AboutUsPage;
