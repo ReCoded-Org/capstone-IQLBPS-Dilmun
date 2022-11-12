ab0e7becf4d2634706fd721af8916ff62a5c8bdc
viyan-najmaldin <najmadinviyan@gmail.com>
2022-11-12 20:17:46 +0300

    change the colors

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -21,7 +21,7 @@ const Sort = () => {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+        <Menu.Button className="group inline-flex justify-center text-sm font-medium text-primary hover:text-gray-900">
           Sort
           <ChevronDownIcon
             className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
@@ -39,7 +39,7 @@ const Sort = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-background shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {sortOptions.map((option) => (
               <Menu.Item key={option.name}>
@@ -48,7 +48,7 @@ const Sort = () => {
                     href={option.href}
                     className={classNames(
                       option.current
-                        ? 'font-medium text-gray-900'
+                        ? 'font-medium text-primary'
                         : 'text-gray-500',
                       active ? 'bg-gray-100' : '',
                       'block px-4 py-2 text-sm'
