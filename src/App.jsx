4bdaa6b6ef04686842fb0d3ca54af627af233fbc
karzan <karzanluqman99@gmail.com>
2022-11-12 14:40:11 +0300

    removed FAQ page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import SearchBar from './components/SearchBar/SearchBar';
 import SignInPage from './Pages/SignInPage/SignInPage';
 import SignUpPage from './Pages/SignUpPage/SignUpPage';
 import HomePage from './Pages/HomePage/HomePage';
-import FAQPage from './Pages/FAQPage/FAQPage';
+// import FAQPage from './Pages/FAQPage/FAQPage';
 
 
 
@@ -25,7 +25,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/faq" element={<FAQPage />} />
+        {/* <Route path="/faq" element={<FAQPage />} /> */}
       </Routes>
       <Footer />
     </div>
