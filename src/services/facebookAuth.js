50c828a12094c74e4ac537335b0caa494547a1d7
ayubabdullah <55099143+ayubabdullah@users.noreply.github.com>
2022-11-12 20:42:49 +0000

    update facebookAuth

--- src/services/facebookAuth.js
+++ src/services/facebookAuth.js
@@ -4,19 +4,10 @@ import { auth } from '../firebase-config';
 const handleFacebookAuth = async () => {
   const provider = new FacebookAuthProvider();
   try {
-    const {user} = await signInWithPopup(auth, provider);
+    const { user } = await signInWithPopup(auth, provider);
     console.log(user);
   } catch (error) {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const { email } = error.customData;
-    // The AuthCredential type that was used.
-    const credential = FacebookAuthProvider.credentialFromError(error);
-
-    console.log(errorCode, errorMessage, email, credential);
-    // ...
+    console.log(error);
   }
 };
 
