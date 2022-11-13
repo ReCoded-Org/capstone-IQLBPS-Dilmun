ee691a2156aba1c9d56a0e0cf236537601cf057a
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-13 21:38:03 +0200

    added lg:col-span-3

--- src/components/ItemsDisplay/ItemsDisplay.jsx
+++ src/components/ItemsDisplay/ItemsDisplay.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const ItemsDisplay = () => {
     return(
-        <div className='grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6' data-testid='itemsdisplay-div'>
+        <div className='grid lg:grid-cols-4 lg:col-span-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6' data-testid='itemsdisplay-div'>
             {/* Add the items cards */}
+            {/* <p>1</p>
             <p>1</p>
             <p>1</p>
             <p>1</p>
             <p>1</p>
             <p>1</p>
-            <p>1</p>
-            <p>1</p>
+            <p>1</p> */}
         </div>
     );
 };
