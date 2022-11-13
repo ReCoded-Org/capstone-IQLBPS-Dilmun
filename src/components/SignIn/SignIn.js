922029b13d8113bff25b1edcad14a2826e88035c
AyubDev <ayubgardy898@gmail.com>
2022-11-13 15:38:47 +0300

    fix eslint error on console.log

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -24,6 +24,7 @@ function SignIn() {
   const userData = useSelector(user);
   const userError = useSelector(error);
   const userStatus = useSelector(status);
+  // eslint-disable-next-line no-console
   console.log(userData, userError, userStatus);
   const {
     register,
