cd61cb0964a70ba179eb472cbbca74f42ec30b97
karzan <karzanluqman99@gmail.com>
2022-11-14 01:51:34 +0300

    Footer padding-test: updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,8 @@ import { Link } from 'react-router-dom';
 
 import {
   HOME_ROUTE,
-  //     CONTACT_ROUTE,
   ABOUT_ROUTE,
   FAQ_ROUTE,
-  //     PROFILE,
 } from '../../route';
 
 const Footer = () => {
