17184f2298b06b941c05bf3a381526f50143ea6f
Kutay Kağan Özen <kutay.kagan.ozenn@gmail.com>
2022-11-13 16:38:32 +0300

    Fix: Fix Router in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './i18n'
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
@@ -12,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </Provider> 
   </React.StrictMode>
 );
