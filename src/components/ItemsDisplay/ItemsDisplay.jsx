30989a7a11d246340fa715d709f69da79603b286
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-13 17:24:55 +0200

    done

--- src/components/ItemsDisplay/ItemsDisplay.jsx
+++ src/components/ItemsDisplay/ItemsDisplay.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const ItemsDisplay = () => {
     return(
         <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6' data-testid='itemsdisplay-div'>
-            {/* <p>1</p>
+            {/* Add the items cards
+            <p>1</p>
             <p>1</p>
             <p>1</p>
             <p>1</p>
