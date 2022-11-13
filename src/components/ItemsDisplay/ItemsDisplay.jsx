bc0989f81548c36da91f301e8f051c5abee1a8a6
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-13 16:43:15 +0200

    making it responsive

--- src/components/ItemsDisplay/ItemsDisplay.jsx
+++ src/components/ItemsDisplay/ItemsDisplay.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const ItemsDisplay = () => {
     return(
-        <div className=''>
-
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-6'>
+            <div className='bg-red-400'>1</div>
+            <div className='bg-gray-400'>1</div>
+            <div className='bg-black-400'>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
         </div>
     );
 };
