a35c851cd1123fa9827642659e60b73b31e11e34
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-13 17:09:32 +0200

    done with the tests

--- src/components/ItemsDisplay/ItemsDisplay.jsx
+++ src/components/ItemsDisplay/ItemsDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ItemsDisplay = () => {
     return(
-        <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6' data-testid='itemsdisplay-div'>
             <p>1</p>
             <p>1</p>
             <p>1</p>
