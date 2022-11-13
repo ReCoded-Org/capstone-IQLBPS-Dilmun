cc30817af57614ea319b110c9ecd41c35671a1c4
AyubDev <ayubgardy898@gmail.com>
2022-11-13 15:34:08 +0300

    Fix testing problems

--- src/components/SignIn/SignIn.test.js
+++ src/components/SignIn/SignIn.test.js
@@ -1,12 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { store } from '../../app/store';
 import SignIn from './SignIn';
 
 test('Sign In Page Renders Correctly', () => {
   render(
-    <Router>
-      <SignIn />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <SignIn />
+      </Router>
+    </Provider>
   );
   expect(screen.getByTestId('sign-in')).toMatchSnapshot();
 });
