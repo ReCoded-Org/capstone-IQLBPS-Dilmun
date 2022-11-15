e8650a513b096ca8b07005ac3b70db1ccb46278e
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-11-15 14:24:22 +0200

    done

--- src/components/ItemsDisplay/ItemsDisplay.jsx
+++ src/components/ItemsDisplay/ItemsDisplay.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
+import ItemsCards from './ItemsCards'
 
 const ItemsDisplay = () => {
     return(
         <div className='grid lg:grid-cols-4 lg:col-span-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6' data-testid='itemsdisplay-div'>
             {/* Add the items cards */}
-            {/* <p>1</p>
-            <p>1</p>
-            <p>1</p>
-            <p>1</p>
-            <p>1</p>
-            <p>1</p>
-            <p>1</p> */}
         </div>
     );
 };
