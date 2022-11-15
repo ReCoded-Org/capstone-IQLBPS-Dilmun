4ae3dc599003da59d30852be990581001e41798c
sozi <sozyarnadir@gmail.com>
2022-11-15 23:13:49 +0300

    Created test files

--- src/components/AddItem/AddItemButton.test.js
+++ src/components/AddItem/AddItemButton.test.js
@@ -4,7 +4,5 @@ import AddItemButton from "./AddItemButton.jsx";
 
 it('render correctly', () => {
     const { queryByPlaceholderText, getByText, queryByAltText } = render( < AddItemButton / > )
-    expect(getByText('Welcome to Dilmun')).toBeTruthy()
-    expect(getByText('Your Way to Get Quality Products!')).toBeTruthy()
-
+    expect(getByText('Add Item')).toBeTruthy()
 })
