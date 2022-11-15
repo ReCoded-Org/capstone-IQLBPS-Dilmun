e3e2e60c3b1beab1e05986f9f3471c06d660c7e5
dania <daniadeyar@gmail.com>
2022-11-15 15:19:05 +0300

    fix redux slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,9 @@
-import {
-  configureStore
-} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../Features/Users/userSlice';
-
 
 export const store = configureStore({
   reducer: {
     counter: 0,
-    user: userReducer
+    user: userReducer.reducer,
   },
 });
