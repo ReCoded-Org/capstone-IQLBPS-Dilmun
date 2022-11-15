4f6a5cbf77cc302ca0d676c1f71efc7db09299a7
dania <daniadeyar@gmail.com>
2022-11-15 22:11:34 +0300

    snapshot upated

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,14 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { render } from "@testing-library/react";
-import NavBar from "./NavBar";
-
-
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import NavBar from './NavBar';
+import { Provider } from 'react-redux';
+import { store } from '../../app/store';
 
 it('render NavBar correctly', () => {
-    const {getByRole} = render( 
+  const { navbar } = renderer
+    .create(
+      <Provider store={store}>
         <Router>
-         <NavBar />
-        </Router>);
-        expect(getByRole("navigation")).toMatchSnapshot()
-    })
+          <NavBar />
+        </Router>
+      </Provider>
+    )
+    .toJSON();
+  expect(navbar).toMatchSnapshot();
+});
