56a8178d6af7e018be585872d68830f96100cc26
sozi <sozyarnadir@gmail.com>
2022-11-15 23:17:17 +0300

    recreated test files

--- src/components/AddItem/AddItemButton.jsx
+++ src/components/AddItem/AddItemButton.jsx
@@ -6,13 +6,11 @@ function AddItemButton() {
       <div className="btn__container">
         <button
           type="button"
-          className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full"
-        >
+          className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded-full">
           Add Item
         </button>
       </div>
     </Link>
   );
 }
-
 export default AddItemButton;
--- src/components/AddItem/AddItemButton.test.js
+++ src/components/AddItem/AddItemButton.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import AddItemButton from "./AddItemButton.jsx";
 
-it('render correctly', () => {
-    const { queryByPlaceholderText, getByText, queryByAltText } = render( < AddItemButton / > )
-    expect(getByText('Add Item')).toBeTruthy()
+test('Add item button component rendered correctly', () => {
+    render( < AddItemButton / > );
+    expect(screen.getByTestId('add-item-button')).toMatchSnapshot()
 })
