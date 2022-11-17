76ce1624a314101c6b95b321948e5ce5eac4e21e
Omar Bassatni <omarbassatni@gmail.com>
2022-11-17 18:40:17 +0200

    Fixed on medium and small screens

--- src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
+++ src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
@@ -9,21 +9,21 @@ import ItemCard from './ItemCard'
 const SignedInUsersHomePage = () => {
     const userData = useSelector(user)
     return (
-        <div className='h-screen bg-background text-primary flex flex-col p-4 items-center justify-between'>
-            <div>
-                <h1 className='text-5xl font-bold'>Hello, {userData.firstName} {userData.lastName}</h1>
-                <div className='text-center text-2xl mt-2'>
+        <div className='lg:h-screen bg-background text-primary flex flex-col p-4 items-center justify-between'>
+            <div className='mb-3 md:mb-0 border-b border-secondary pb-2 md:pb-6 w-full'>
+                <h1 className='text-2xl md:text-5xl font-bold text-center'>Hello, {userData.firstName} {userData.lastName}</h1>
+                <div className='text-center md:text-2xl mt-2'>
                     <span>Can&apos;t see what you need?</span>
                     <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={PRODUCT_ROUTE}>Press Here</Link>
                 </div>
-                <div className='text-2xl text-center mt-3'>
+                <div className='md:text-2xl text-center mt-3'>
                     <span>Or add your own item</span>
                     <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={ADD_ITEM_ROUTE}>Add Item</Link>
                 </div>
             </div>
             <div>
-                <h1 className='text-3xl font-bold'>Some of our products</h1>
-                <div className='grid grid-cols-3 gap-6 mt-2'>
+                <h1 className='text-2xl md:text-3xl font-bold'>Some of our products</h1>
+                <div className='flex flex-col md:grid grid-cols-3 gap-6 mt-2'>
                     <ItemCard imgSrc="https://placeimg.com/300/300/any" />
                     <ItemCard imgSrc="https://placeimg.com/300/300/animals" />
                     <ItemCard imgSrc="https://placeimg.com/300/300/architecture" />
