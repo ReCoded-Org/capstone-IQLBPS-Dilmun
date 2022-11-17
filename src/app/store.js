16c75ba4c8852c54c2863d8d30ffde4bafc17023
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-18 00:56:33 +0200

    added item reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,10 +3,11 @@ import {
   configureStore
 } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice';
+import itemReducer from '../features/slices/item'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-
+    item: itemReducer
   },
 });
