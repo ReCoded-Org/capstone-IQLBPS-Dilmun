9dde108fde86ed5ec551d5f105b733809c4981b8
Hiba <eng.lilil92@gmail.com>
2022-11-18 22:37:02 +0300

    styles edited

--- src/Pages/SignedInUsersHomePage/ItemCard.js
+++ src/Pages/SignedInUsersHomePage/ItemCard.js
@@ -7,7 +7,7 @@ const ItemCard = ({ imgSrc }) => {
             <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Backpack</h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <button type='submit' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button type='submit' className="inline-flex items-center text-background px-3 py-2 rounded bg-secondary ml-2 hover:bg-primary transition duration-300">
                     Read more
                 </button>
             </div>
--- src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
+++ src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
@@ -10,15 +10,19 @@ const SignedInUsersHomePage = () => {
     const userData = useSelector(user)
     return (
         <div data-testid='SignedInUseresHomePage' className='bg-background text-primary flex flex-col items-center justify-between'>
-            <div className=' bg-home-page-for-signed-in-users bg-cover w-full min-h-[30vh] md:min-h-[60vh] text-background p-5'>
+            <div className=' bg-home-page-for-signed-in-users bg-cover w-full min-h-[40vh] md:min-h-[7p0vh] text-background p-5'>
                 <div className=' mb-3 md:mb-0 pt-5 md:pt-14 md:pb-6 w-full'>
                     <h1 className='text-2xl md:text-5xl font-bold text-center'>Welcome back, </h1>
                     <h1 className='text-2xl md:text-5xl font-bold text-center'> {userData.firstName} {userData.lastName}</h1>
                     <h3 className='text-l md:text-2xl font-semi-bold text-center'> Dilmun is your best choice to buy or sell anything</h3>
+                    <div className='text-center md:text-2xl m-4'>
+                        <Link className='px-3 py-2 rounded bg-secondary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={PRODUCT_ROUTE}>More Products</Link>
+                        <Link className='px-3 py-2 rounded bg-secondary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={ADD_ITEM_ROUTE}>Add New Item</Link>
+                    </div>
                 </div>
             </div>
-            <div className=' mb-3 md:mb-0 border-b border-secondary pb-2 md:pb-6 w-full'>
-            <div className='text-center md:text-2xl mt-2'>
+            {/* <div className=' mb-3 md:mb-0 border-b border-secondary pb-2 md:pb-6 w-full'>
+                    <div className='text-center md:text-2xl mt-2'>
                         <span>Can&apos;t see what you need?</span>
                         <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={PRODUCT_ROUTE}>Press Here</Link>
                     </div>
@@ -26,7 +30,7 @@ const SignedInUsersHomePage = () => {
                         <span>Or add your own item</span>
                         <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={ADD_ITEM_ROUTE}>Add Item</Link>
                     </div>
-            </div>
+            </div> */}
             <div>
                 <h1 className='text-2xl md:text-3xl font-bold mt-2 mb-2'>Some of our products</h1>
                 <div className='flex flex-col md:grid grid-cols-3 gap-6 mt-2'>
