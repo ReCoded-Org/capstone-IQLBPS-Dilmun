8910bf06d2a0f79b11420b2bd1ba2046039b73e2
Omar Bassatni <omarbassatni@gmail.com>
2022-11-18 22:36:20 +0200

    added new home page in animation provider

--- src/components/animations/AnimationProvider.js
+++ src/components/animations/AnimationProvider.js
@@ -1,6 +1,8 @@
 import React from "react";
-import {Routes,Route,useLocation} from 'react-router-dom'
-import {AnimatePresence} from 'framer-motion'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import { useSelector } from "react-redux";
+import _ from 'lodash';
 import AboutUsPage from '../../Pages/AboutUsPage/AboutUsPage';
 import SignInPage from '../../Pages/SignInPage/SignInPage';
 import SignUpPage from '../../Pages/SignUpPage/SignUpPage';
@@ -9,12 +11,16 @@ import AddItemPage from '../../Pages/AddItemPage/AddItemPage';
 import FilterPage from '../../Pages/FilterPage/FilterPage';
 import FaqPage from '../../Pages/FaqPage/FaqPage';
 
+import { user } from "../../features/user/userSlice";
+import SignedInUsersHomePage from "../../Pages/SignedInUsersHomePage/SignedInUsersHomePage";
+
 function AnimationProvider() {
-    const location = useLocation()
-    return (
-        <AnimatePresence>
-        <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<HomePage />} />
+  const location = useLocation()
+  const userData = useSelector(user)
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        {!_.isEmpty(userData) ? <Route path="/" element={<SignedInUsersHomePage />} /> : <Route path='/' element={<HomePage />} />}
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/products" element={<FilterPage />} />
@@ -22,8 +28,8 @@ function AnimationProvider() {
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/add-item" element={<AddItemPage />} />
       </Routes>
-      </AnimatePresence>
-    )
+    </AnimatePresence>
+  )
 }
 
 export default AnimationProvider
