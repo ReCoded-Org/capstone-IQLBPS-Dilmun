c54b3eeb1af1425106d77f151537698f62774192
Hiba <eng.lilil92@gmail.com>
2022-11-18 22:13:04 +0300

    background cover and welcome message edit

--- src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
+++ src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
@@ -9,17 +9,23 @@ import ItemCard from './ItemCard'
 const SignedInUsersHomePage = () => {
     const userData = useSelector(user)
     return (
-        <div data-testid='SignedInUseresHomePage' className='bg-background text-primary flex flex-col p-4 items-center justify-between'>
-            <div className='mb-3 md:mb-0 border-b border-secondary pb-2 md:pb-6 w-full'>
-                <h1 className='text-2xl md:text-5xl font-bold text-center'>Hello, {userData.firstName} {userData.lastName}</h1>
-                <div className='text-center md:text-2xl mt-2'>
-                    <span>Can&apos;t see what you need?</span>
-                    <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={PRODUCT_ROUTE}>Press Here</Link>
+        <div data-testid='SignedInUseresHomePage' className='bg-background text-primary flex flex-col items-center justify-between'>
+            <div className=' bg-home-page-for-signed-in-users bg-cover w-full min-h-[30vh] md:min-h-[60vh] text-background p-5'>
+                <div className=' mb-3 md:mb-0 pt-5 md:pt-14 md:pb-6 w-full'>
+                    <h1 className='text-2xl md:text-5xl font-bold text-center'>Welcome back, </h1>
+                    <h1 className='text-2xl md:text-5xl font-bold text-center'> {userData.firstName} {userData.lastName}</h1>
+                    <h3 className='text-l md:text-2xl font-semi-bold text-center'> Dilmun is your best choice to buy or sell anything</h3>
                 </div>
-                <div className='md:text-2xl text-center mt-3'>
-                    <span>Or add your own item</span>
-                    <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={ADD_ITEM_ROUTE}>Add Item</Link>
-                </div>
+            </div>
+            <div className=' mb-3 md:mb-0 border-b border-secondary pb-2 md:pb-6 w-full'>
+            <div className='text-center md:text-2xl mt-2'>
+                        <span>Can&apos;t see what you need?</span>
+                        <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={PRODUCT_ROUTE}>Press Here</Link>
+                    </div>
+                    <div className='md:text-2xl text-center mt-3'>
+                        <span>Or add your own item</span>
+                        <Link className='p-1 rounded bg-tertiary ml-2 hover:text-tertiary hover:bg-primary transition duration-300' to={ADD_ITEM_ROUTE}>Add Item</Link>
+                    </div>
             </div>
             <div>
                 <h1 className='text-2xl md:text-3xl font-bold mt-2 mb-2'>Some of our products</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         "signin-background": "url('https://cdn.discordapp.com/attachments/1031834305703460906/1037447936616964136/bg.png')",
-        "hero-bg": "url('https://images.unsplash.com/photo-1561715276-a2d087060f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
+        "hero-bg": "url('https://images.unsplash.com/photo-1561715276-a2d087060f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')",
+        "home-page-for-signed-in-users": "url('https://media.istockphoto.com/id/1250152532/photo/online-shopping-and-digital-marketing-concept-woman-using-digital-tablet-with-shopping-cart.jpg?s=612x612&w=0&k=20&c=RiLlRrGvpi0iLTdGS2JSQx95gySojYUWM5EAzz3lCCo=')"
       },
       colors: {
         primary: '#16425B',
