1be6d0320efc3b5a5e4cbb0a95111c6551060da8
Marwa Hakmi <marwahakmi@gmail.com>
2022-11-19 11:50:16 +0200

    test: modify add item form test to pass

--- src/components/AddItemForm/AddItemForm.test.jsx
+++ src/components/AddItemForm/AddItemForm.test.jsx
@@ -1,7 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import AddItemForm from './AddItemForm';
+import { store } from '../../app/store';
 
 test('Add item form component rendered correctly', () => {
-    render(<AddItemForm/>);
+    render(
+      <Provider store={store}>
+        <AddItemForm />
+      </Provider>
+    );
     expect(screen.getByTestId('add-item-form')).toMatchSnapshot()
 })
