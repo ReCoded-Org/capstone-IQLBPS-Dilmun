ec0c3812976f0668991a8898710b2ab00d6c864f
Omar Bassatni <omarbassatni@gmail.com>
2022-11-19 13:30:43 +0200

    deleted commented code

--- src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.test.js
+++ src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.test.js
@@ -1,17 +1,3 @@
-// import React from "react";
-// import renderer from "react-test-renderer";
-// import ItemCard from "./ItemCard"
-// import SignedInUsersHomePage from "./SignedInUsersHomePage";
-
-// test(' SignedInUsersHomePage renders without crashing ' , () => {
-//     const tree = renderer.create(
-//         <SignedInUsersHomePage>
-//             <ItemCard />
-//         </SignedInUsersHomePage>
-//      ).toJSON();
-//     expect(tree).toMatchSnapshot();
-
-// })
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -24,8 +10,8 @@ test('SignedInUsersHomePage renders without crashing', () => {
     <Provider store={store}>
       <Router>
         <SignedInUsersHomePage>
-            <ItemCard />
-        </SignedInUsersHomePage>      
+          <ItemCard />
+        </SignedInUsersHomePage>
       </Router>
     </Provider>
   );
