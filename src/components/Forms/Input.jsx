cf3b51d08e2cfdbb530f128a44a4e4bc80b2b1b2
Marwa Hakmi <marwahakmi@gmail.com>
2022-11-20 23:45:54 +0200

    feat: style disabled input

--- src/components/Forms/Input.jsx
+++ src/components/Forms/Input.jsx
@@ -30,6 +30,7 @@ const Input = React.forwardRef(
           className={[
             'relative border-2 rounded-md mb-2 text-background border-tertiary',
             errors ? 'border-red-500' : '',
+            disabled ? 'disabled' : '',
           ].join(' ')}
         >
           <input
@@ -37,6 +38,7 @@ const Input = React.forwardRef(
               'outline-none w-full rounded bg-transparent text-sm transition-all duration-200 ease-in-out p-2',
               active ? 'pt-6' : '',
               errors ? 'error' : '',
+              disabled ? 'disabled' : '',
             ].join(' ')}
             name={name}
             id={name}
@@ -54,6 +56,7 @@ const Input = React.forwardRef(
               'absolute top-0 left-0 flex items-center text-tertiary text-opacity-50 p-2 transition-all duration-200 ease-in-out',
               active ? 'text-xs' : 'text-sm',
               errors ? 'error' : '',
+              disabled ? 'disabled' : '',
             ].join(' ')}
             htmlFor={name}
           >
