76b292cac9afa20af28e318588b02a08dc987dc3
viyan-najmaldin <najmadinviyan@gmail.com>
2022-11-22 18:24:30 +0300

    add form validation

--- src/Pages/ContactUsPage/ContactUs.js
+++ src/Pages/ContactUsPage/ContactUs.js
@@ -1,4 +1,14 @@
+import { useForm } from 'react-hook-form';
+
 const ContactUs = () => {
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {};
+
   return (
     <div className="bg-white text-gray-100 px-8 py-12">
       <div className="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto text-gray-900">
@@ -17,7 +27,7 @@ const ContactUs = () => {
             />
           </div>
         </div>
-        <div className="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
             <span className="uppercase text-sm text-gray-600 font-bold">
               Full Name
@@ -26,7 +36,15 @@ const ContactUs = () => {
               className="w-full bg-background text-gray-900 mt- p-3 rounded-lg focus:outline-none focus:shadow-outline"
               type="text"
               placeholder=""
+              {...register('name', {
+                required: '* Enter your name please',
+              })}
             />
+            {errors.name && (
+              <p className="text-red-800 font-semibold text-[12px] ">
+                {errors.name?.message}
+              </p>
+            )}
           </div>
           <div className="mt-5">
             <span className="uppercase text-sm text-gray-600 font-bold">
@@ -34,14 +52,32 @@ const ContactUs = () => {
             </span>
             <input
               className="w-full bg-background text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="text"
+              type="email"
+              {...register('email', {
+                required: '* Email address is required',
+              })}
             />
+            {errors.email && (
+              <p className="text-red-800 font-semibold text-[12px]">
+                {errors.email?.message}
+              </p>
+            )}
           </div>
           <div className="mt-5">
             <span className="uppercase text-sm text-gray-600 font-bold">
               Message
             </span>
-            <textarea className="w-full h-40 bg-background text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" />
+            <textarea
+              className="w-full h-40 bg-background text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+              {...register('feedback', {
+                required: '* This Input is required',
+              })}
+            />
+            {errors.feedback && (
+              <p className="text-red-800 font-semibold text-[12px]">
+                {errors.feedback?.message}
+              </p>
+            )}
           </div>
           <div className="mt-5">
             <button
@@ -51,7 +87,7 @@ const ContactUs = () => {
               Send Message
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
