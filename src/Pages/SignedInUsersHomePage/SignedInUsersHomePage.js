12464118a8badb1a71f75e7085bf01d88a24e81a
Omar Bassatni <omarbassatni@gmail.com>
2022-11-22 15:37:16 +0200

    Fixed some bugs

--- src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
+++ src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
@@ -19,7 +19,7 @@ const SignedInUsersHomePage = () => {
                     <h1 className='text-2xl md:text-6xl font-bold text-center text-shadow'>Welcome back, </h1>
                     <h1 className='text-2xl md:text-6xl font-bold text-center mb-2'> {userData.firstName} {userData.lastName}</h1>
                     <h3 className='md:text-3xl font-bold text-center'> Dilmun is your best choice to buy or sell anything</h3>
-                    <div className='flex justify-center gap-2'>
+                    <div className='sm:flex justify-center gap-2 mt-2'>
                         <span className=' flex items-center justify-center gap-2 font-bold md:text-2xl'>Can&apos;t find what you need? Check our list<Link to={PRODUCT_ROUTE}><BsFillCartFill size={40} className='hover:text-background transition duration-300' /></Link> </span>
                         <span className=' flex items-center justify-center gap-2 font-bold md:text-2xl'>or add an item <Link to={ADD_ITEM_ROUTE}><IoIosAddCircle size={40} className='hover:text-background transition duration-300' /></Link> </span>
                     </div>
