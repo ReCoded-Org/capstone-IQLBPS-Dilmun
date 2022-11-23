fbeda6471bb1aceb87ef45458170a01f0d098c69
Marwa Hakmi <50196875+MARWAHAKMI@users.noreply.github.com>
2022-11-23 19:36:40 +0200

    40 feature implement add item functionality (#143)
    * init item slice
    
    * added item reducer to store
    
    * feat: init add item functionality
    
    * save user object to redux
    
    * fix: save uid and email to redux onload
    
    * feat: add item to items and users collections and save it in redux
    
    * Revert "feat: add item to items and users collections and save it in redux"
    
    This reverts commit b3d0bcd8ad914a6a90175fe3b3137537fa49895c.
    
    * feat: add item to items and users collections and save it in redux
    
    * feat: display error message if there
    
    * refactor: files structre for slices
    
    * test: modify add item form test to pass
    
    * feat: disable price input when type of item is donated
    
    * test: modify add item page test to pass
    
    * updating user data with address input
    
    * conidyional rendering applied
    
    * removed smth
    
    * conditional setDoc
    
    * removed conditional rendering and fixed file input
    
    * fix: make input active onFocus
    
    * fixed conditional rendering and file input
    
    * fix: reset form after submitting
    
    * cleanup: move update user obj to firestore login into user slice
    
    * cleanup: remove unused code and snap test file updated
    
    * feat: style disabled input
    
    * added $
    
    * fix: saving user's address into redux and firestore
    
    * removed email from user doc
    
    * removed price input error
    
    * price value fixed
    
    * changed naming
    
    * firebase storage setup
    
    * feat: get img url from firebase storage and save it in firestore
    
    * cleanup: code smells
    
    * add: redirecting on submit
    
    * tests updated
    
    * refactor: move CSF logic into item slice
    fix: navigating to prev page after adding item successfully
    
    * new date format
    
    Co-authored-by: Sarah Abou Dalal <sarahaboudalal@gmail.com>
    Co-authored-by: Sarah Abou Dalal <109027707+sarahaboudalal@users.noreply.github.com>
    Co-authored-by: Kutay Kağan Özen <kutay.kagan.ozenn@gmail.com>

--- src/components/AddItemForm/AddItemForm.test.jsx
+++ src/components/AddItemForm/AddItemForm.test.jsx
@@ -1,7 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AddItemForm from './AddItemForm';
+import { store } from '../../app/store';
 
 test('Add item form component rendered correctly', () => {
-    render(<AddItemForm/>);
+    render(
+      <Provider store={store}>
+        <Router>
+          <AddItemForm />
+        </Router>
+      </Provider>
+    );
     expect(screen.getByTestId('add-item-form')).toMatchSnapshot()
 })
