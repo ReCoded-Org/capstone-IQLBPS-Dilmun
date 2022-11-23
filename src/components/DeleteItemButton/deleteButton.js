9e68e211e4551a2ea69eaf2efef5fcbe6d541a9d
Oussama Masri <oussamamasri16120@gmail.com>
2022-11-23 21:19:52 +0200

    Fix: fixed some stuff

--- src/components/DeleteItemButton/deleteButton.js
+++ src/components/DeleteItemButton/deleteButton.js
@@ -6,7 +6,8 @@ function deleteItemButton() {
             <span 
               className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-primary group-hover:translate-x-0 ease">
             <svg 
-              className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg>
+              className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            </svg>
             </span>
             <span 
               className="absolute flex items-center justify-center w-full h-full text-primary transition-all duration-300 transform group-hover:translate-x-full ease">
