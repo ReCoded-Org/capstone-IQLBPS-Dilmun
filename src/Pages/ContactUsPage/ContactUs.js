a5b9e466620ec6c9da34c8adde244595213d9ab3
viyan-najmaldin <najmadinviyan@gmail.com>
2022-11-23 19:23:43 +0300

    center the image

--- src/Pages/ContactUsPage/ContactUs.js
+++ src/Pages/ContactUsPage/ContactUs.js
@@ -13,13 +13,13 @@ const ContactUs = () => {
     <div className="bg-white text-gray-100 px-8 py-12">
       <div className="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto text-gray-900">
         <div className="flex flex-col justify-between">
-          <div className="self-center text-center">
+          <div className="text-center">
             <h2 className="text-4xl lg:text-5xl font-bold leading-tight text-primary ">
               Lets talk about everything!
             </h2>
             <div className="text-gray-600 mt-8 ">Give us your feedback</div>
           </div>
-          <div className="mt-8 text-center">
+          <div className="mt-8 text-center self-center">
             <img
               className="h-44 sm:h-56 md:h-72"
               src="https://alshifaeye.org/assets/images/contact-us.png"
