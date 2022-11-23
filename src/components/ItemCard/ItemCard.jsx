86b0d839df41c92307975158a7aef2e4aaf25552
sozi <sozyarnadir@gmail.com>
2022-11-23 21:40:41 +0300

    draft to test

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -10,12 +10,13 @@ function ItemCard () {
         <p className="text-gray-700 text-base">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
         </p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
+        <div className="px-6 pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#book</span>
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#reading</span>
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bookshelf</span>
       </div>
+      </div>
+    
     </div>
   
   );
