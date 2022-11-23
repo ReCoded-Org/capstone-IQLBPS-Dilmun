742d275aa0525b5ddb4c858df60881aee6dabee8
Omar Bassatni <omarbassatni@gmail.com>
2022-11-23 20:02:22 +0200

    Changed some items to recent items and centering it

--- src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
+++ src/Pages/SignedInUsersHomePage/SignedInUsersHomePage.js
@@ -14,7 +14,7 @@ const SignedInUsersHomePage = () => {
 
     return (
         <div data-testid='SignedInUseresHomePage' className='bg-background text-primary flex flex-col items-center justify-between'>
-            <div className=' bg-home-page-for-signed-in-users flex flex-col justify-center bg-cover bg-center w-full min-h-[50vh] text-white p-5 shadow-lg'>
+            <div className=' bg-home-page-for-signed-in-users backdrop-blur-md flex flex-col justify-center bg-cover bg-center w-full max-h-[30vh] text-white p-5 shadow-lg'>
                 <div className=' mb-3 md:mb-0 pt-5 md:pt-14 md:pb-6 w-full'>
                     <h1 className='text-2xl md:text-6xl font-bold text-center text-shadow'>Welcome back, </h1>
                     <h1 className='text-2xl md:text-6xl font-bold text-center mb-2'> {userData.firstName} {userData.lastName}</h1>
@@ -27,8 +27,8 @@ const SignedInUsersHomePage = () => {
 
                 </div>
             </div>
-            <div className='mt-5'>
-                <h1 className='text-4xl font-semibold mb-4 ml-4'>Some of our Products</h1>
+            <div className='mt-5 max-h-[60vh]'>
+                <h1 className='text-4xl font-bold mb-4 ml-4 text-center'>Most Recent Items</h1>
                 <div className='w-full px-5 lg:px-0 overflow-x-scroll slider flex flex-col lg:flex-row gap-4'>
                     <ItemCard />
                     <ItemCard />
