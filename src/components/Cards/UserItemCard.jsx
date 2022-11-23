4816e1fafa2fb7da5bbf92ccbf3541a15ecdb5e8
Marwa Hakmi <marwahakmi@gmail.com>
2022-11-24 00:44:05 +0200

    feat: added action btns to card

--- src/components/Cards/UserItemCard.jsx
+++ src/components/Cards/UserItemCard.jsx
@@ -1,3 +1,5 @@
+import { FaEdit, FaTrashAlt } from 'react-icons/fa';
+
 function UserItemCard() {
   return (
     <div className="my-10 relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-primary">
@@ -8,11 +10,12 @@ function UserItemCard() {
           className="rounded-xl"
         />
       </div>
-      <div className="w-full md:w-2/3 bg-background flex flex-col space-y-2 p-3 rounded-xl">
+      <div className="w-full md:w-2/3 bg-background flex flex-col space-y-2 p-3 rounded-xl justify-between">
         <div className="flex justify-between item-center">
           <p className="text-gray-500 font-medium hidden md:block">Item Type</p>
-          <div className="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
-            categories
+          <div className="flex gap-2 justify-between items-center">
+            <FaTrashAlt className="h-6 w-6 text-red-500" />
+            <FaEdit className="h-6 w-6 text-primary" />
           </div>
         </div>
         <h3 className="font-black text-gray-800 md:text-3xl text-xl">
@@ -21,9 +24,13 @@ function UserItemCard() {
         <p className="md:text-lg text-gray-500 text-base min-h-20">
           Description of item Here...
         </p>
-        <p className="absolute bottom-4 text-xl font-black text-gray-800">
-          $110
-        </p>
+
+        <div className=" flex justify-between item-center">
+          <p className=" text-xl font-black text-gray-800">$110</p>
+          <div className="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
+            categories
+          </div>
+        </div>
       </div>
     </div>
   );
