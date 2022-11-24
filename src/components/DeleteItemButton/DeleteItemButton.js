3210378f5f66e4fa28170c94a53891573299dae8
Oussama Masri <oussamamasri16120@gmail.com>
2022-11-24 06:19:31 +0200

    Fix: fixed the Delete Button

--- src/components/DeleteItemButton/DeleteItemButton.js
+++ src/components/DeleteItemButton/DeleteItemButton.js
@@ -2,18 +2,18 @@ function DeleteItemButton() {
     return (
             < button
               type = "button"
-              className = "relative inline-flex items-center justify-center p-1 px-1 py-2 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-md group" >
+              className = "relative inline-flex items-center justify-center p-1 px-1 py-2 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full group" >
             <span 
               className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-red-600 group-hover:translate-x-0 ease">
             <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045145545250971779/recycle-bin.png' alt='' className="w-5 h-5 " />
             </span>
             <span 
-              className="absolute flex items-center justify-center w-full h-full text-primary transition-all duration-300 transform group-hover:translate-x-full ease">
+              className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform group-hover:translate-x-full ease">
               <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045146842305933402/trash.png' alt='' className="w-5 h-5 " />
             </span>
             <span 
               className="relative invisible">
-                Delete
+               <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045146842305933402/trash.png' alt='' className="w-5 h-5 " />
             </span>
             </button>
     )
