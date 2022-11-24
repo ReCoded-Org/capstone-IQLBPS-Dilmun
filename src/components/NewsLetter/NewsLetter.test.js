1e2e4ba9e9b3f411075b4ef4357d0cc7fcdfa4c4
Sarah Abou Dalal <109027707+sarahaboudalal@users.noreply.github.com>
2022-11-24 22:43:41 +0200

    Fix colors in some components (#162)
    * changed colors of feature section
    
    * proof sectioncolors fixed
    
    * newsletter comp restyled
    
    * about us page
    
    * fixed footer
    
    * user item card

--- src/components/NewsLetter/NewsLetter.test.js
+++ src/components/NewsLetter/NewsLetter.test.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import NewsLetter from ".";
+import { render, screen } from "@testing-library/react";
+import NewsLetter from "./NewsLetter";
 
 
-it('render correctly', () => {
-    const { queryByPlaceholderText, getByText, queryByAltText} = render(<NewsLetter />)
-
-    expect(queryByPlaceholderText('Write your email here')).toBeTruthy()
-    expect(getByText('Subscribe to our newsletter and stay updated')).toBeTruthy()
-    expect(queryByAltText('email')).toBeTruthy()
-    expect(getByText("Subscribe to our Newsletter !")).toMatchSnapshot(`
-    <h1 className='text-[20px] sm:text-[40px] mb-3 sm:mb-0'>Subscribe to our <br /> Newsletter !</h1>
-  `);
+it('render NewsLetter correctly', () => {
+  render(<NewsLetter />)
+  expect(screen.getByTestId('newsletter')).toMatchSnapshot()
 })
