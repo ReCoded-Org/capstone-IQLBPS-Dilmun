f4fd55331df2c47419ca3b7896cb54f5c30cbb8f
mawa <mawahama05@gmail.com>
2022-11-24 14:25:24 -0800

    colors fixed

--- src/components/Profile/Form.js
+++ src/components/Profile/Form.js
@@ -28,41 +28,41 @@ function Form(){
       }
 
     return ( 
-    <form  className=" w-full ">
+    <form  className=" bg-tertiary ">
         <div className="bg- py-6 px-10 sm:max-w-md w-full rounded-md">
                             <div className="sm:text-3xl text-2xl font-semibold text-center w-full text-primary  mb-12">
                             Edit your profile 
                             </div>
                         {/* Image upload component */ }
                         <div className="flex justify-center mt-6">
-                            <div className="max-w-2xl rounded-lg shadow-xl bg-gray-50">
+                            <div className="max-w-2xl rounded-lg shadow-xl bg-background border-dashed hover:border-primary">
                                 <div className="m-4">
-                                    <p className="inline-block mb-3 text-gray-500">Profile picture upload</p>
+                                    <p className="inline-block mb-3 text-primary">Profile picture upload</p>
                                     <div className="flex items-center justify-center w-full">
-                                        <div className="flex flex-col w-80 h-24 border-4 border-dashed hover:bg-terinary hover:border-gray-300">
+                                        <div className="flex flex-col w-80 h-24 border-4 border-dashed hover:border-primary">
                                             <div className="flex flex-col items-center justify-center pt-7">
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-400 group-hover:text-gray-600"
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-primary group-hover:text-background"
                                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path 
                                                     d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                                 </svg>
-                                                <p className="pt-1 text-sm tracking-wider text-gray-40 group-hover:text-gray-600">Attach a file</p>
+                                                <p className="pt-1 text-sm tracking-wider text-primary">Attach a file</p>
                                         </div>
                                         <input type="file" className="opacity-0" {...register('profilePic')}/>
                                     </div>
                                 </div>
                                 {/* Background Image */}
                                 <div className="m-4">
-                                    <p className="inline-block mb-3 text-gray-500">Background picture upload</p>
+                                    <p className="inline-block mb-3 text-primary">Background picture upload</p>
                                     <div className="flex items-center justify-center w-full">
-                                        <div className="flex flex-col w-80 h-24 border-4 border-dashed hover:bg-terinary hover:border-gray-300">
+                                        <div className="flex flex-col w-80 h-24 border-4 border-dashed hover:border-primary">
                                             <div className="flex flex-col items-center justify-center pt-7">
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-400 group-hover:text-gray-600"
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-primary group-hover:text-background"
                                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path 
                                                     d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                                 </svg>
-                                                <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">Attach a file</p>
+                                                <p className="pt-1 text-sm tracking-wider text-primary">Attach a file</p>
                                         </div>
                                         <input type="file" className="opacity-0" {...register('backgroundPic')}/>
                                     </div>
@@ -70,12 +70,12 @@ function Form(){
                                 </div>
                         </div>
                         <div className="flex justify-center p-2">
-                            <button type="button"className=" rounded-full  p-3 w-full sm:w-56  hover:bg-terinary text-primary text-lg font-semibold " >Upload</button>
+                            <button type="button"className=" rounded-full  p-3 w-full sm:w-56  hover:bg-tertiary text-primary text-lg font-semibold " >Upload</button>
                         </div>
                     </div>
                 </div> 
                         <div className="flex flex-col justify-center gap-2">
-                                <input type="text" onChange={e=>setfirstName(e.target.value)} className=" w-full p-1 bg-background rounded-lg mt-3 "  placeholder=" First name " {...register('firstName')}/>
+                                <input type="text" onChange={e=>setfirstName(e.target.value)} className=" w-full p-1 bg-background rounded-lg mt-3 "  placeholder=" First name " {...register('firstName',{ required: true, maxLength: 10 })}/>
                                    {error?
                                     <p>First name can not be empty</p>:""
                                    }
@@ -92,10 +92,10 @@ function Form(){
                                 {error?
                                     <p>City can not be empty</p>:""
                                    } 
-                                <button type="button" onClick={handleClick} className=" rounded-full  p-3 w-full sm:w-56  bg-[#D9DCD6] hover:bg-neutral-300  text-primary text-lg font-semibold " >
+                                <button type="button" onClick={handleClick} className=" rounded-full  p-3 w-full sm:w-56 bg-background hover:border hover:border-primary hover:bg-tertiay text-primary text-lg font-semibold " >
                                 Save
                                 </button>
-                                <button type="button" className=" rounded-full  p-3 w-full sm:w-56  bg-[#D9DCD6] hover:bg-neutral-300  text-primary text-lg font-semibold " >
+                                <button type="button" className=" rounded-full  p-3 w-full sm:w-56  bg-background hover:border hover:border-primary text-primary text-lg font-semibold " >
                                 Cancel
                                 </button>
                             
