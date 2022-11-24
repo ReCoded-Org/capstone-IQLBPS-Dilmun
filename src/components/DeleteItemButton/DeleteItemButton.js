a5b438e4dae45d0cf826f3fb5473d4710da0808f
Oussama Masri <oussamamasri16120@gmail.com>
2022-11-24 03:23:07 +0200

    Fix: redesign the delete Button

--- src/components/DeleteItemButton/DeleteItemButton.js
+++ src/components/DeleteItemButton/DeleteItemButton.js
@@ -1,17 +1,15 @@
-import { BsTrash } from 'react-icons/bs';
-
 function DeleteItemButton() {
     return (
             < button
               type = "button"
-              className = "relative inline-flex items-center justify-center p-2 px-3 py-2 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-md group" >
+              className = "relative inline-flex items-center justify-center p-1 px-1 py-2 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-md group" >
             <span 
               className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-red-600 group-hover:translate-x-0 ease">
-            <BsTrash className="w-5 h-5" />
+            <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045145545250971779/recycle-bin.png' alt='' className="w-5 h-5 " />
             </span>
             <span 
               className="absolute flex items-center justify-center w-full h-full text-primary transition-all duration-300 transform group-hover:translate-x-full ease">
-                Delete
+              <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045146842305933402/trash.png' alt='' className="w-5 h-5 " />
             </span>
             <span 
               className="relative invisible">
