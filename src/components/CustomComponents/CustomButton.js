09879c637774cf895f9f3d844e45a349d3e58506
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-24 20:12:47 +0200

    newsletter comp restyled

--- src/components/CustomComponents/CustomButton.js
+++ src/components/CustomComponents/CustomButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const CustomButton = ({ children, paddingX, paddingY }) => {
-    const className = `bg-tertiary ${paddingX} ${paddingY} w-full sm:w-auto  text-white font-semibold rounded-lg hover:shadow-lg hover:bg-secondary transition duration-500 cursor-pointer`
+    const className = `bg-tertiary ${paddingX} ${paddingY} w-full lg:w-auto text-white font-semibold rounded-lg hover:shadow-lg hover:bg-secondary transition duration-500 cursor-pointer`
     return (
         <button type='submit' className={className}>{children}</button>
     )
