f6190d430fc08a101e5d0f07beb89d22b4633bdd
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-24 23:48:42 +0200

    test deleted

--- src/components/ItemCard/ItemCardSection.jsx
+++ src/components/ItemCard/ItemCardSection.jsx
@@ -19,7 +19,6 @@ export default function ItemCardSection() {
 
   return (
     <div
-      data-testid="item-card"
       className="w-full grid gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 content-center items-center justify-items-center bg-background py-6 my-2"
     >
       <div>
