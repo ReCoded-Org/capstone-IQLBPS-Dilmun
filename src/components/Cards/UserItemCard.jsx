8807559c1e2ed3027c580d77ba864a45912895bf
Marwa Hakmi <marwahakmi@gmail.com>
2022-11-24 09:24:34 +0200

    modify edit item button and added tooltip to it

--- src/components/Cards/UserItemCard.jsx
+++ src/components/Cards/UserItemCard.jsx
@@ -1,7 +1,5 @@
-import {
-  FaEdit,
-  //  FaTrashAlt
-} from 'react-icons/fa';
+import { BiEdit } from 'react-icons/bi';
+import Tooltip from '../Tooltip/Tooltip';
 import DeleteItemButton from '../DeleteItemButton/DeleteItemButton';
 
 function UserItemCard(props) {
@@ -12,15 +10,16 @@ function UserItemCard(props) {
   return (
     <div className="my-10 relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-primary">
       <div className="w-full md:w-1/3 bg-transparent grid place-items-center">
-        <img src={file} alt="tailwind logo" className="rounded-xl" />
+        <img src={file} alt={title} className="rounded-xl" />
       </div>
       <div className="w-full md:w-2/3 bg-background flex flex-col space-y-2 p-3 rounded-xl justify-between">
         <div className="flex justify-between item-center">
           <p className="text-gray-500 font-medium">{type}</p>
           <div className="flex gap-2 justify-between items-center">
-            {/* <FaTrashAlt className="h-6 w-6 text-red-500" /> */}
             <DeleteItemButton />
-            <FaEdit className="h-6 w-6 text-primary" />
+            <Tooltip text="Edit Item">
+              <BiEdit className="h-8 w-8 text-primary" />
+            </Tooltip>
           </div>
         </div>
         <h3 className="font-black text-gray-800 md:text-3xl text-xl">
@@ -30,8 +29,8 @@ function UserItemCard(props) {
           {description}
         </p>
 
-        <div className=" flex justify-between item-center">
-          <p className=" text-xl font-black text-gray-800">${price}</p>
+        <div className="flex justify-between item-center">
+          <p className="text-xl font-black text-gray-800">${price}</p>
           <div className="flex justify-center items-center">
             {categories.map((category) => (
               <span
