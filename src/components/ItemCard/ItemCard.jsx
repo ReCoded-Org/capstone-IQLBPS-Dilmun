de3e077aeb09e122c0d51656e36a21e6bf0ef3e7
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-24 21:38:56 +0200

    removed items from dependency array

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -13,7 +13,7 @@ function ItemCard() {
       setItems(itemData);
  }
     func()
-  }, [items]);
+  }, []);
   return (
     <div className="w-full grid gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 content-center items-center justify-items-center bg-background py-6 my-2">
       {items.map((item) => {
