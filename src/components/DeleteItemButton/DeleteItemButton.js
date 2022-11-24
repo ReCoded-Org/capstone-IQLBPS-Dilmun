508a3da1cab168780c5a77b29513597653b29d9a
Oussama Masri <oussamamasri16120@gmail.com>
2022-11-24 06:41:03 +0200

    Fix : made the button bigger

--- src/components/DeleteItemButton/DeleteItemButton.js
+++ src/components/DeleteItemButton/DeleteItemButton.js
@@ -5,15 +5,15 @@ function DeleteItemButton() {
               className = "relative inline-flex items-center justify-center p-1 px-1 py-2 overflow-hidden transition duration-300 ease-out rounded-full group" >
             <span 
               className="absolute inset-0 flex items-center justify-center w-full h-full duration-300 -translate-x-full bg-red-600 group-hover:translate-x-0 ease">
-            <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045145545250971779/recycle-bin.png' alt='' className="w-5 h-5 " />
+            <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045145545250971779/recycle-bin.png' alt='' className="w-6 h-6 " />
             </span>
             <span 
               className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform group-hover:translate-x-full ease">
-              <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045146842305933402/trash.png' alt='' className="w-5 h-5 " />
+              <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045146842305933402/trash.png' alt='' className="w-6 h-6 " />
             </span>
             <span 
               className="relative invisible">
-               <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045146842305933402/trash.png' alt='' className="w-5 h-5 " />
+               <img src='https://cdn.discordapp.com/attachments/1043477388589277264/1045146842305933402/trash.png' alt='' className="w-6 h-6 " />
             </span>
             </button>
     )
