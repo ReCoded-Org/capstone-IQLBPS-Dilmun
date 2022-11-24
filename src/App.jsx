8f261f41ec76285dfbec153b3528915b94ac381b
Marwa Hakmi <50196875+MARWAHAKMI@users.noreply.github.com>
2022-11-24 10:08:56 +0200

    32 create profile page profile item card (#154)
    * Add: added the ProfileItemPage (phase-1)
    
    * Fix: fixing some bugs(phase1)
    
    * Fix : fixed some bugs(phase2)
    
    * Fix : fixed some bugs(phase3)
    
    * feat: initial design for user's item card
    
    * feat: added action btns to card
    
    * Fix: redesign the delete Button
    
    * Fix: fixed the Delete Button
    
    * Add: added the DeleteItemButton to the UserItemCard
    
    * Delete: removed some unwated things
    
    * Fix : made the button bigger
    
    * Delete : deleted some unwanted files
    
    * Fix: removed some unwanted files
    
    * feat: added Tooltip to icon button
    
    * Initialize item card props to use them in JSX
    
    * add Mock Item Data to pass it as props to user item card
    
    * modify edit item button and added tooltip to it
    
    * test: snapshot test added to user item card
    
    Co-authored-by: Oussama Masri <oussamamasri16120@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,25 @@ import {
   user,
 } from './features/slices/user';
 import AnimationProvider from './components/animations/AnimationProvider';
+import { UserItemCard } from './components/Cards';
+
+const MOCK_ITEM = {
+  title: 'Title of Item Goes Here',
+  file: 'https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  description: 'Description of item Here...',
+  price: 110,
+  type: 'Item Type',
+  categories: ['Men', 'Women', 'Kids'],
+};
 
 function App() {
   const dispatch = useDispatch();
 
-  const userData = useSelector(user)
-  const errorData = useSelector(error)
-  const statusData = useSelector(status)
+  const userData = useSelector(user);
+  const errorData = useSelector(error);
+  const statusData = useSelector(status);
   // eslint-disable-next-line no-console
-  console.log(userData, errorData, statusData)
+  console.log(userData, errorData, statusData);
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
@@ -39,8 +49,8 @@ function App() {
   return (
     <div className="App  ">
       <NavBar />
-
       <AnimationProvider />
+      <UserItemCard item={MOCK_ITEM} />
       <Footer />
     </div>
   );
