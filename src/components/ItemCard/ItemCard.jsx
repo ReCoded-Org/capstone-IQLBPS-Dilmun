761bd599de557b7df031d0bea6156508b4110a03
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-24 20:20:33 +0200

    component rendered

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -8,8 +8,11 @@ function ItemCard() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getItemList(itemData));
-    setItems(itemData);
+ const func = async()=> {
+    await dispatch(getItemList(itemData));
+      setItems(itemData);
+ }
+    func()
   }, [items]);
   return (
     <div data-testid='item-card' className="w-full grid gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 content-center items-center justify-items-center bg-background py-6 my-2">
