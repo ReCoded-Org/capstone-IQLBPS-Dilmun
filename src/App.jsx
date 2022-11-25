5e7b66840f67970112a972ce615e3e162a4d1fa9
Omar Bassatni <omarbassatni@gmail.com>
2022-11-25 21:44:32 +0200

    deleted console logs

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
   const errorData = useSelector(error);
   const statusData = useSelector(status);
   // eslint-disable-next-line no-console
-  console.log(userData, errorData, statusData);
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
