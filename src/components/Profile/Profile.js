b4ae7750241bcc16851733d8e65c28612cf9e8f3
Marwa Hakmi <marwahakmi@gmail.com>
2022-11-25 11:24:17 +0200

    replace custom card with user item card

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,8 +7,18 @@ import { auth } from '../../firebase-config';
 import { ADD_ITEM_ROUTE } from '../../route';
 import defaultProfileImg from '../../assets/img/defaultProfileImg.jpg';
 import defaultBGImg from '../../assets/img/defaultBGImg.jpg';
-import CustomItemCard from '../CustomComponents/CustomItemCard';
+// import CustomItemCard from '../CustomComponents/CustomItemCard';
+import { UserItemCard } from '../Cards';
 import Form from "./Form";
+
+const MOCK_ITEM = {
+  title: 'Title of Item Goes Here',
+  file: 'https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  description: 'Description of item Here...',
+  price: 110,
+  type: 'Item Type',
+  categories: ['Men', 'Women', 'Kids'],
+};
 
 
 function Profile() {
@@ -51,7 +61,7 @@ function Profile() {
           </h1>
           <h3 className="font-semibold text-[14px] sm:text-[18px] lg:text-[24px] text-primary m-1">
             {' '}
-            {userData.address.country},{userData.address.city}
+            {userData?.address.country},{userData?.address.city}
           </h3>
 
           <h5 className="font-semibold text-[14px] sm:text-[18px] lg:text-[24px] text-primary flex flex-row m-1">
@@ -89,12 +99,12 @@ function Profile() {
               My Items
             </h3>
           </div>
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 p-5">
-            <CustomItemCard />
-            <CustomItemCard />
-            <CustomItemCard />
-            <CustomItemCard />
-            <CustomItemCard />
+          <div className="grid md:grid-cols-2 gap-4 p-5">
+            <UserItemCard item={MOCK_ITEM} />
+            <UserItemCard item={MOCK_ITEM} />
+            <UserItemCard item={MOCK_ITEM} />
+            <UserItemCard item={MOCK_ITEM} />
+            <UserItemCard item={MOCK_ITEM} />
           </div>
           <Link to={ADD_ITEM_ROUTE}>
             {' '}
