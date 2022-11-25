f2b8433e8551c64d997658c62599fd9c57d7cfd1
karzan <karzanluqman99@gmail.com>
2022-11-25 22:13:55 +0300

    draft:before a break

--- src/components/AboutUs/AboutUsHeader.jsx
+++ src/components/AboutUs/AboutUsHeader.jsx
@@ -9,13 +9,16 @@ const AboutUsHeader = () => {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-                <h1 className='text-4xl font-bold mt-0 mb-6'>
+                <h1 className='text-4xl text-primary font-bold mt-0 mb-6'>
                     Meet Our Team
                 </h1>
             </div>
-            <div className=" flex flex-row justify-around">
-                <div className="">something</div>
-                <div className="max-w-sm rounded-[32px] overflow-hidden shadow-lg"><img src={abtImg} alt='Team' /></div>
+            <div className=" flex lg:flex-row md:flex-col sm:flex-col justify-around">
+                <div className="">
+                    <p className="text-secondary text-lg p-12 pt-24">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
+                <div className="max-sm rounded-[32px] overflow-hidden shadow-lg"><img src={abtImg} alt='Team' /></div>
                 {/* {team.map((member) => {
           return <MemberCard key={member.id} member={member} />; */}
                 {/* })} */}
