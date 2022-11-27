71ee50d477ad92a9c638f950559332cc5afc34f5
Marwa Hakmi <marwahakmi@gmail.com>
2022-11-27 22:58:04 +0200

    fix: action buttons

--- src/components/Cards/UserItemCard.jsx
+++ src/components/Cards/UserItemCard.jsx
@@ -12,16 +12,18 @@ function UserItemCard(props) {
   // destructure the item object to use values in the JSX
   const { title, file, description, price, type, category, id } = item;
 
-  const handleDelete = (itemId, userId) => {
+  const handleDelete = ( itemId, userId) => {
     dispatch(deleteItem({ itemId, userId }));
   };
 
   return (
-    <Link to={`/product/${id}`}>
-      <div className="my-10 relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl border border-background bg-primary">
+    
+    <div className="my-10 relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl border border-background bg-primary">
+      <Link to={`/product/${id}`}>
         <div className="w-full md:w-1/2 bg-transparent grid place-items-center">
           <img src={file} alt={title} className="rounded-xl w-full h-auto  " />
         </div>
+        </Link>
         <div className="w-full md:w-2/3 bg-background flex flex-col space-y-2 p-3 rounded-xl justify-between">
           <div className="flex justify-between item-center">
             <p className="text-secondary font-medium text-lg">{type}</p>
