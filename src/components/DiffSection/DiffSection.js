c1aeee8517474b8b6bb33582feeccbe5ccd2fc8d
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-12-15 16:58:52 +0200

    bg of hero changed

--- src/components/DiffSection/DiffSection.js
+++ src/components/DiffSection/DiffSection.js
@@ -5,7 +5,7 @@ import OverlapText from "./OverlapText"
 function DiffSection(){
 
     return (
-            <div className="shadow-md bg-primary w-full h-96 relative">
+            <div className="shadow-md bg-background w-full h-96 relative">
                 <video src={Dilmun} autoPlay loop muted className="w-full h-full object-cover opacity-60 drop-shadow-2xl absolute">
                     <track kind="captions"/>
                 </video> 
